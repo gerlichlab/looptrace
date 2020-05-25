59d84587c85524a1dd7d96ab89caa8cea617b191
Kai Sandvold Beckwith <kai.beckwith@embl.de>
2020-05-25 15:39:50 +0200

    Tracing updates
    Small updates to tracing functions. Moved all scripts to main folder to get rid of path issues.

--- tracer.py
+++ tracer.py
@@ -8,15 +8,16 @@ import os
 import yaml
 import numpy as np
 import pandas as pd
-from image_processing_functions.gaussfit import fitSymmetricGaussian3D,fitSymmetricGaussian3DMLE
+from gaussfit import fitSymmetricGaussian3D, fitSymmetricGaussian3DMLE
 from read_roi import read_roi_zip, read_roi_file
 from skimage.filters import gaussian
 from scipy.stats import mode
 import scipy.ndimage as ndi
-from image_processing_functions import image_processing_functions as ip
-from image_processing_functions import tracing_functions as tr
+import image_processing_functions as ip
+import tracing_functions as tr
 import h5py
 import tifffile as tiff
+from joblib import Parallel, delayed
 
 class Tracer:
     def __init__(self, config_path):
@@ -45,15 +46,15 @@ class Tracer:
         
         #Find lists of images and ROIs, and match them so that only images
         #with matching ROIs are included.
+        self.trace_id = 0
+        
         image_folder = self.config['image_folder']
         roi_folder = self.config['roi_folder']
-        match_idx = self.config['match_idx']
-        image_template = ['.tiff']
-        roi_template = ['.zip']
+        image_template = self.config['image_template']
+        roi_template = self.config['roi_template']
         image_list = ip.all_matching_files_in_subfolders(image_folder,image_template)
-        roi_list = ip.all_matching_files_in_subfolders(image_folder,roi_template)
-        image_list = ip.match_file_lists(roi_list, image_list, match_idx)        
-        
+        roi_list = ip.all_matching_files_in_subfolders(roi_folder,roi_template)
+        image_list = ip.match_file_lists(roi_list, image_list)        
         # res is a list of tuples, each with a list of lists for each image/roi set
         res = [self.tracing_3d(image_path, roi_path) for image_path, roi_path in zip(image_list, roi_list)]
         
@@ -85,12 +86,21 @@ class Tracer:
         min_nuc_size=self.config['min_nuc_size']
         nuc_ch=self.config['nuc_ch']
         nuc_frame=self.config['nuc_frame']
+        
+        bead_ch = self.config['bead_ch']
+        bead_thresh = self.config['bead_threshold']       
+        bead_int = self.config['bead_intensity']
+        bead_points = self.config['bead_points']
+        
         trace_ch=self.config['trace_ch']
         crop_z = self.config['crop_z']
-        roi_image_size=self.config['roi_image_size']
-        man_qc=self.config['man_qc']
-        # Read in images and ROIs.
+        roi_image_size = self.config['roi_image_size']
+        man_qc = self.config['man_qc']
+        
+        # Read in tif image from with course drift correction, and
+        # reorganize from imageJ TZCYX to Python TCZYX format.
         image=ip.read_tif_image(image_path)
+        image=np.moveaxis(image, 1, 2)
         print('Loaded image ', image_path)
         
         #Define image name and final image size for stacking.
@@ -98,8 +108,17 @@ class Tracer:
         exp_shape=[image.shape[0]]+roi_image_size
         
         #Detect nuclei and data about nuclei.
-        nuc_labels, nuc_props = ip.detect_nuclei(image[nuc_frame,:,nuc_ch,:,:], min_nuc_size)
-        
+        nuc_labels, nuc_props = ip.detect_nuclei(image[nuc_frame,nuc_ch], min_nuc_size)
+        print('Nuclei detection completed.')
+        #Detect subpixel drift correction from fiducials.
+        drift = Parallel(n_jobs=-2)(delayed(ip.drift_corr_multipoint_cc)(image[0],
+                                             image[i],
+                                             bead_ch,
+                                             bead_thresh,
+                                             bead_int,
+                                             points=bead_points)
+                 for i in range(image.shape[0]))
+        print('Drift correction completed.')
         #Read ImageJ ROIs from .roi or .zip file.
         if roi_path[-3:] == 'roi':
             rois=read_roi_file(roi_path)
@@ -109,17 +128,26 @@ class Tracer:
             rois=[rois[k] for k in list(rois)]
             
         #Initialize loop over ROIs.
+        num_rois=len(rois)
         all_coords=[]
         all_imgs=[]
         roi_id=0
         for roi in rois:
+            #Expand roi to even numbers for consistency.
+            roi['height']=roi['height']+roi['height']%2
+            roi['width']=roi['width']+roi['width']%2
+            
             # Generate cropped image and expand with zeros from ROI and config specification
-            crop_pos=(0, roi['position']['slice']-8, roi['top'], roi['left'])
+            crop_pos=(0, max(0,roi['position']['slice']-8), roi['top'], roi['left'])
             crop_size=(image.shape[0], crop_z, roi['height'], roi['width'])
-            crop_img=ip.crop_at_pos(image[:,:,trace_ch,:,:], crop_pos, crop_size)
+            crop_img=ip.crop_at_pos(image[:,trace_ch,:,:,:], crop_pos, crop_size)
+            
+            # Find transposition coordinates to transpose fit to 
+            # correct place in ROI.
             transp_z=(exp_shape[1]-crop_z)/2
-            transp_y=(exp_shape[2]-roi['height']-1)/2
-            transp_x=(exp_shape[3]-roi['width']-1)/2
+            transp_y=(exp_shape[2]-roi['height'])/2
+            transp_x=(exp_shape[3]-roi['width'])/2
+            
             #Find out which nucleus the ROI belongs to.
             nuc_roi=ip.crop_at_pos(nuc_labels, crop_pos[1:], crop_size[1:])
             try:
@@ -140,8 +168,9 @@ class Tracer:
                 dog=gaussian(img,1)-gaussian(img,10)
                 max_ind=list(np.unravel_index(np.argmax(dog, axis=None), dog.shape))
                 roi_coords.append([trace_id, image_name, roi_id, nuc_id, t]+
-                                  [*fitSymmetricGaussian3D(img,3,max_ind)[0]]+
-                                  [transp_z, transp_y, transp_x])
+                                  [*fitSymmetricGaussian3D(img,1,max_ind)[0]]+
+                                  [transp_z, transp_y, transp_x]+
+                                  [*drift[t]])
             
             # Add parameters to a list of pd DataFrames and images to list of images.
             all_coords.append(pd.DataFrame(roi_coords, columns=["trace_ID",
@@ -154,57 +183,94 @@ class Tracer:
                                                                 "z_px",
                                                                 "y_px",
                                                                 "x_px",
-                                                                "sigma_w",
                                                                 "sigma_z",
+                                                                "sigma_xy",
                                                                 'transp_z',
                                                                 'transp_y',
-                                                                'transp_x']))
+                                                                'transp_x',
+                                                                'drift_z',
+                                                                'drift_y',
+                                                                'drift_x']))
             
-            all_imgs.append(ip.pad_to_shape(crop_img, exp_shape))
+            # Pad and drift correct the spot image.
+            
+            crop_img=ip.pad_to_shape(crop_img, exp_shape)
+            crop_img=np.stack([ndi.shift(crop_img[i], s) for 
+                               i, s in enumerate(drift)])
+            
+            all_imgs.append(crop_img)
             roi_id+=1
             self.trace_id+=1
-        
+            print('ROI {} of {} finished tracing.'.format(roi_id,num_rois))
         #Apply quality control and calibration metrics to all traces.
         traces=pd.concat(all_coords)
-        traces['z_px']=traces['z_px']+traces['transp_z']
-        traces['y_px']=traces['y_px']+traces['transp_y']
-        traces['x_px']=traces['x_px']+traces['transp_x']
-        traces=traces.drop(columns=['transp_z','transp_y','transp_x'])
+        traces['z_px']=traces['z_px']+traces['transp_z']+traces['drift_z']
+        traces['y_px']=traces['y_px']+traces['transp_y']+traces['drift_y']
+        traces['x_px']=traces['x_px']+traces['transp_x']+traces['drift_x']
+        traces=traces.drop(columns=['transp_z','transp_y','transp_x', 
+                                    'drift_z', 'drift_y', 'drift_x'])
         traces['z']=traces['z_px']*self.config['z_nm']
         traces['y']=traces['y_px']*self.config['xy_nm']
         traces['x']=traces['x_px']*self.config['xy_nm']
-        traces['sigma_w']=traces['sigma_w']*self.config['xy_nm']
         traces['sigma_z']=traces['sigma_z']*self.config['z_nm']
+        traces['sigma_xy']=traces['sigma_xy']*self.config['xy_nm']
         traces=traces.set_index(['trace_ID', 'img_name', 'roi_ID', 'cell_ID', 'frame'])
-        traces['QC']=traces.apply(self.tracing_qc,axis=1)
+        traces=self.reapply_QC(traces)
         
-        #Stack list of images to single array.
+        #Stack list of trace images to single array.
         imgs=np.stack(all_imgs)
         print('Processed image ', image_path)
         return traces, all_imgs
     
     def tracing_qc(self, row):
+        '''
+        Function to set QC value of each fit based on 
+        settings from config file.
+        '''
+        
         A_to_BG=self.config['A_to_BG']
-        sigma_w_max=self.config['sigma_w_max']
+        sigma_xy_max=self.config['sigma_xy_max']
         sigma_z_max=self.config['sigma_z_max']
         man_qc=self.config['man_qc']
-                
-        if row['BG']>(A_to_BG*row['A']):
+        if row['A']<(A_to_BG*row['BG']):
             return 0
-        elif row['sigma_w'] > sigma_w_max or row['sigma_z'] > sigma_z_max:
+        elif row['sigma_xy'] > sigma_xy_max or row['sigma_z'] > sigma_z_max:
             return 0
         elif row.name[4] in man_qc:
+            return 0
+        elif row['x_px']<0 or row['y_px'] < 0 or row['z_px']<0:
+            return 0
+        elif row['x_px']>100 or row['y_px'] > 100 or row['z_px'] > 100:
             return 0
         else:
             return 1
         
-    def reapply_QC(self,traces, pwds):
+    def group_mean_qc(self, row, groups):
+        '''
+        Function to set QC value of each row
+        based on group calculation, in this case 
+        number of nm away from group mean each point can be.
+        Preserves original QC, can only change 1 to 0.
+        '''
+        
+        min_groups=groups-1000
+        max_groups=groups+1000
+        if max_groups.iloc[row.name[0]]['z']>row['z']<min_groups.iloc[row.name[0]]['z']:
+            return 0
+        if max_groups.iloc[row.name[0]]['y']>row['y']<min_groups.iloc[row.name[0]]['y']:
+            return 0
+        if max_groups.iloc[row.name[0]]['x']>row['x']<min_groups.iloc[row.name[0]]['x']:
+            return 0
+        if row['QC'] == 0:
+            return 0
+        else:
+            return 1
+        
+    def reapply_QC(self,traces):
         traces['QC']=traces.apply(self.tracing_qc,axis=1)
-        # Recalculate pairwise distances for all traces.
-        points = [tr.points_from_df_nan(df)[0] for _, df in traces.groupby(level=0)]
-        pwds = [cdist(p, p) for p in points]
-        pwds = np.stack(pwds)
-        return traces, pwds
+        group_means=traces.query('QC==1').groupby(level=0).mean()
+        traces['QC']=traces.apply(self.group_mean_qc, args=(group_means,), axis=1)
+        return traces
     
     def save_data(self, traces=None, imgs=None, pwds=None, pairs=None, config=None, suffix=''):
         output_folder=self.config['output_folder']
---
+++ tracing_functions.py
@@ -0,0 +1,364 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 09:19:04 2020
+
+@author: ellenberg
+"""
+import itertools
+import pandas as pd
+import numpy as np
+import matplotlib
+from plotly.offline import iplot
+import plotly.graph_objs as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+from scipy import interpolate
+from scipy.spatial.distance import cdist
+from scipy.cluster.hierarchy import single, dendrogram
+#from pycpd import RigidRegistration
+
+def points_for_overlay(traces):
+    points_frame=traces.reset_index()[['trace_ID','frame','z_px','y_px','x_px']]
+    points_frame[['y_px','x_px']]=points_frame[['y_px','x_px']].clip(lower=0, upper=64)
+    points_frame[['z_px']]=points_frame[['z_px']].clip(lower=0, upper=16)
+    points=points_frame.to_numpy()
+    return points
+
+def pwd_calc(traces):
+    '''
+    Parameters
+    ----------
+    traces : pd DataFrame with trace data.
+
+    Returns
+    -------
+    pwds : Pair-wise distance matrixes for traces as an 3-dim numpy array.
+    '''
+    
+    points = [points_from_df_nan(df)[0] for _, df in traces.groupby(level=0)]
+    pwds = [cdist(p, p) for p in points]
+    pwds = np.stack(pwds)
+    return pwds
+
+def tracing_length_qc(traces, min_length=0):
+    '''
+    Parameters
+    ----------
+    traces : pd DataFrame with trace data.
+    pwds : 3dim np array from pwd_calc.
+    min_length : Int, minimum length of trace to pass QC
+
+    Returns
+    -------
+    traces : pd DataFrame with shorter traces removed.
+    pwds : 3-dim np array of pwds that passed length QC.
+
+    '''
+    grouped=traces.groupby(level=0)
+    traces_long=grouped.filter(lambda x : x['QC'].sum()>=min_length)
+    return traces_long
+
+def trace_analysis(traces, pwds):
+    '''
+    Calculates pairwise trace similarity based on :
+        - MSE of points after rigid alignment
+        - MSE of pairwise distance matrices
+        - Pearson's correlation coeff of pwds.
+        
+    Parameters
+    ----------
+    traces : pd DataFrame with trace data.
+    pwds : 3dim np array from pwd_calc.
+
+    Returns
+    -------
+    output : pd DataFrame with results, including indexes and point 
+                coordinates of original and aligned traces.
+    '''
+    
+    pairwise_trace_idx = list(itertools.combinations(traces.index.get_level_values(0).unique(),2))
+    pairwise_pwd_idx = list(itertools.combinations(range(pwds.shape[0]),2))
+    res = []
+    for ((idx1, idx2), (idx_p1, idx_p2)) in zip(pairwise_trace_idx,pairwise_pwd_idx):
+        A, A_idx, B, B_idx, B_aligned, B_aligned_idx, mse = rigid_transform_3D(traces.loc[idx1], traces.loc[idx2])
+        pwd_mse = mat_corr_mse(pwds[idx_p1],pwds[idx_p2])
+        pwd_pcc = mat_corr_pcc(pwds[idx_p1],pwds[idx_p2])        
+        res.append([idx1, idx2, A, A_idx, B, B_idx, B_aligned, B_aligned_idx, mse, pwd_mse, pwd_pcc])
+    columns=['idx1', 'idx2', 'A', 'A_idx', 'B', 'B_idx', 'B_aligned', 'B_aligned_idx', 'aligned_mse', 'pwd_mse', 'pwd_pcc']
+    output=pd.DataFrame(res,columns=columns)
+    return output
+
+def trace_clustering(paired, metric):
+    '''
+    Calculates clusters nased on similarity metrics of 
+    pairwise analysis of traces. Also plots dendrogram of clusters.
+    
+    Parameters
+    ----------
+    paired : Paired analysis DataFrame, output of trace_analysis
+    metric : One of the similarity metrics from trace_analysis:
+            - 'aligned_mse'
+            - 'pwd_mse'
+            - 'pwd_pcc'
+    Returns
+    -------
+    Z : Clustering matrix based on hierarchial clustering (see scipy.cluster.hierarchy.single docs)
+    '''
+    
+    labels=np.unique(np.concatenate((paired['idx1'],paired['idx2'])))
+    if metric == 'aligned_mse' or metric == 'pwd_mse':
+        Z=single(paired[metric])
+    elif metric == 'pwd_pcc':
+        Z=single(1-paired[metric])
+    else:
+        raise ValueError('Inappropriate metric.')
+    
+    dendrogram(Z, labels=labels)
+    return Z
+
+def rigid_transform_3D(df_A, df_B):
+    '''
+    Calculates rigid transformation of two 3d points sets based on:
+    Least-squares fitting of two 3-D point sets. IEEE T Pattern Anal 1987
+    DOI: 10.1109/TPAMI.1987.4767965
+    Only uses points present in both traces for alignment.
+
+    Parameters
+    ----------
+    df_A, df_B : pd DataFrames with single trace data.
+
+    Returns
+    -------
+    Point coordinates and indexes of original and aligned traces,
+    and MSE of alignment.
+    '''
+    
+    A_orig, A_orig_idx = points_from_df(df_A)
+    B_orig, B_orig_idx = points_from_df(df_B)
+    A, idx_A, B, idx_B = matching_points_from_dfs(df_A,df_B)
+    
+    #Need column vectors for calculation
+    A=A.T
+    B=B.T
+    num_rows, num_cols = A.shape;
+
+    if num_rows != 3:
+        raise Exception("matrix A is not 3xN, it is {}x{}".format(num_rows, num_cols))
+
+    [num_rows, num_cols] = B.shape;
+    if num_rows != 3:
+        raise Exception("matrix B is not 3xN, it is {}x{}".format(num_rows, num_cols))
+
+    # find mean column wise and reshape to column vector
+    Ac = np.mean(A, axis=1).reshape((3,1))
+    Bc = np.mean(B, axis=1).reshape((3,1))
+
+    # subtract mean
+    Am = A - Ac
+    Bm = B - Bc
+
+    # dot is matrix multiplication for array
+    H = np.matmul(Am, Bm.T)
+
+    # find rotation
+    U, S, Vt = np.linalg.svd(H)
+    R = np.matmul(Vt.T,U.T)
+
+    # special reflection case 
+    if np.linalg.det(R) < 0:
+        #print("det(R) < R, reflection detected!, correcting for it ...\n");
+        Vt[2,:] *= -1
+        R = np.matmul(Vt.T,U.T)
+
+    t = Bc - np.matmul(R,Ac)
+    B_ = np.matmul(R.T,B-t)
+    mse = ((A - B_)**2).mean()
+    B_aligned = np.matmul(R.T,B_orig.T-t).T
+    return A_orig, A_orig_idx, B_orig, B_orig_idx, B_aligned, B_orig_idx, mse
+
+'''
+def scale_rigid_transform_3D(df_A,df_B):
+    A_orig, A_orig_idx = points_from_df(df_A)
+    B_orig, B_orig_idx = points_from_df(df_B)
+    A, idx_A, B, idx_B = matching_points_from_dfs(df_A,df_B)
+    reg = RigidRegistration(**{'X': A, 'Y': B})
+    B_ = reg.transform_point_cloud(B)
+    mse = ((A - B_)**2).mean()
+    B_aligned = reg.transform_point_cloud(B_orig)
+    return B_aligned, B_orig_idx, mse
+'''
+
+def points_from_df(trace_df):
+    '''
+    Helper function to extract point coordinates from trace dataframe.
+    Only points passing QC during tracing are returned.
+
+    Parameters
+    ----------
+    trace_df : pd DataFrame with trace data.
+
+    Returns
+    -------
+    points : Nx3 np array with trace coordinates.
+    idx : List, Original index of trace coordinates.
+    '''
+    
+    _traces = trace_df[trace_df['QC']==1]
+    points = np.array([_traces['x'].values,_traces['y'].values,_traces['z'].values]).T
+    idx = _traces.index.get_level_values('frame')
+    return points, idx
+
+def points_from_df_nan(trace_df):
+    '''
+    Helper function to extract point coordinates from trace dataframe.
+    All points are returned, but points not passing QC during tracing 
+    are returned as NaN.   
+
+    Parameters
+    ----------
+    trace_df : pd DataFrame with trace data.
+
+    Returns
+    -------
+    points : Nx3 np array with trace coordinates.
+    idx : List, Original index of trace coordinates.
+    '''
+    
+    _traces=trace_df.copy()
+    _traces[_traces['QC']==0] = np.nan
+    points = np.array([_traces['x'].values,_traces['y'].values,_traces['z'].values]).T
+    idx = _traces.index.get_level_values('frame')
+    return points, idx
+
+def matching_points_from_dfs(df_A,df_B):
+    '''
+    Helper function to find points that are common to two traces.
+
+    Parameters
+    ----------
+    df_A, df_B : pd DataFrames with single trace data.
+
+    Returns
+    -------
+    Coordinates (np array) and indexes (list) of the matching points.
+
+    '''
+    df_A_idx=df_A[df_A['QC']==1].index.get_level_values('frame')
+    df_B_idx=df_B[df_B['QC']==1].index.get_level_values('frame')
+    idx=df_A_idx.intersection(df_B_idx)
+    i = pd.IndexSlice
+    points_A, idx_A = points_from_df(df_A.loc[i[:, :, :, idx], i[:]])
+    points_B, idx_B = points_from_df(df_B.loc[i[:, :, :, idx], i[:]])
+    return points_A, idx_A, points_B, idx_B
+
+def spline_interp(points):
+    '''
+    Performs cubic B-spline interpolation on point coordinates.
+
+    Parameters
+    ----------
+    points : n_points X ndim list or array of point coordinates.
+
+    Returns
+    -------
+    fine : 100 X ndim nd array of interpolated points.
+
+    '''
+    
+    tck, u = interpolate.splprep(points, s=0)
+    knots = interpolate.splev(tck[0], tck)
+    u_fine = np.linspace(0,1,num=100)
+    fine = interpolate.splev(u_fine, tck)
+    return fine
+
+def mat_corr_mse(mat1, mat2):
+    '''
+    Calculate mean squared error of two matrices, ignoring nans.
+    '''
+    
+    mse = np.nanmean((mat1-mat2)**2)
+    return mse
+
+def mat_corr_pcc(mat1,mat2):
+    '''
+    Calculate pearson's corr coef of two matrices, ignoring nans.
+    '''
+    mat1_bar=np.nanmean(mat1)
+    mat2_bar=np.nanmean(mat2)
+    
+    pcc_num=np.nansum((mat1-mat1_bar)*(mat2-mat2_bar))
+    pcc_denom=np.sqrt(np.nansum((mat1-mat1_bar)**2)*np.nansum((mat2-mat2_bar)**2))
+    pcc=pcc_num/pcc_denom
+    
+    return pcc
+        
+def plot_traces(traces,idx):
+    '''
+    Helper function for plotting one or several traces in one figure.
+    Also plots spline interpolation between points for visualization.
+    
+    Parameters
+    ----------
+    traces : pd DataFrame with trace data.
+    idx : Int or list of ints with trace_ID of traces to plot.     
+    '''
+    
+    if type(idx) == int:
+        idx=[idx]
+    trace_df=traces[traces['QC']==1]
+    trace_df_sel=pd.concat([trace_df.xs(i) for i in idx])
+    trace_index=trace_df_sel.index.get_level_values('frame')
+    
+    labels=['E'+str(t) for t in trace_index]
+    fig = px.scatter_3d(trace_df_sel, x='x', y='y', z='z',
+              color=labels)
+    for i in idx:
+        interp=spline_interp([trace_df.xs(i)['z'].values,
+                         trace_df.xs(i)['y'].values,
+                         trace_df.xs(i)['x'].values])
+        fig.add_trace(go.Scatter3d(x=interp[2], 
+                                   y=interp[1], 
+                                   z=interp[0],
+                                  mode ='lines',
+                                  showlegend=False))
+    iplot(fig)
+    
+def plot_paired_traces(pair_df, idx):
+    '''
+    Helper function for plotting two aligned traces in one figure.
+    Also plots spline interpolation between points for visualization.
+    
+    Parameters
+    ----------
+    pair_df : pd DataFrame with paired trace analysis, output of trace_analysis.
+    idx : Int, index of pair from paired dataframe. 
+    '''
+    
+    points1=pair_df['A'][idx]
+    idx1=pair_df['A_idx'][idx]
+    points2=pair_df['B_aligned'][idx]
+    idx2=pair_df['B_aligned_idx'][idx]
+
+    z1,y1,x1=points1.T
+    z2,y2,x2=points2.T
+    
+    z_fine1,y_fine1,x_fine1=spline_interp([z1,y1,x1])
+    z_fine2,y_fine2,x_fine2=spline_interp([z2,y2,x2])
+    cmap = px.colors.qualitative.Plotly
+    cmap1 = [cmap[i%10] for i in idx1]
+    cmap2 = [cmap[i%10] for i in idx2]
+    labels1=['E'+str(i) for i in idx1]
+    labels2=['E'+str(i) for i in idx2]
+    fig = go.Figure(data=[go.Scatter3d(x=x1, y=y1, z=z1,
+                                       mode='markers+text', 
+                                       marker_color=cmap1),
+                          go.Scatter3d(x=x_fine1,y=y_fine1,z=z_fine1, 
+                                       mode='lines'),
+                          go.Scatter3d(x=x2, y=y2, z=z2,
+                                       mode='markers+text', 
+                                       marker_color=cmap2),
+                          go.Scatter3d(x=x_fine2,y=y_fine2,z=z_fine2, 
+                                       mode='lines')])
+    
+    iplot(fig)
+    
