c80ce82fdcee1e2e08676b013c8f26274a9b6319
Kai Sandvold Beckwith <kai.beckwith@embl.de>
2020-07-28 08:21:05 +0200

    Update tracing_gui_handler_class.py

--- chromatin_tracing_python/tracing_gui_handler_class.py
+++ chromatin_tracing_python/tracing_gui_handler_class.py
@@ -10,21 +10,21 @@ import chromatin_tracing_python.image_processing_functions as ip
 import pandas as pd
 
 class Handler:
-    def __init__(self, config_path, dc_path):
+    def __init__(self, config_path):
         '''
         Initialize Handler class with config read in from YAML file.
     '''
         
         self.config_path = config_path
         self.config = ip.load_config(config_path)
-        if 
-        self.drift_table = pd.read_csv(dc_path)
+        
+        self.drift_table = None
+        if os.path.exists(default_dc_path):
+            self.drift_table = pd.read_csv(dc_path)
+
         self.images, self.pos_list = ip.images_to_dask(self.config['input_folder'], self.config['image_filetype']+self.config['image_template'])
         self.images_shape = self.images.shape
         
 
         print('Loaded images of shape ', self.images_shape)
         print('Found positions ', self.pos_list)
-
-        self.fit_funcs = {'LS': fitSymmetricGaussian3D, 'MLE': fitSymmetricGaussian3DMLE}
-        self.fit_func = self.fit_funcs[self.config['fit_func']]
