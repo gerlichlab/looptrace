51fe3e12f0408dcb78707962a2de6c3284a643b5
Kai Sandvold Beckwith <kai.beckwith@embl.de>
2020-08-17 21:30:43 +0200

    try new scaling

--- bin/tracing_gui.py
+++ bin/tracing_gui.py
@@ -79,7 +79,7 @@ def main():
                 roi_thread = threading.Thread(target=T.rois_from_spots)
                 roi_thread.start()
             elif values['-IJ_ROI-']:
-                T.roi_table = ip.rois_from_imagej(values['-IJ_ROI_PATH-']) # Oeyvind added ", '.zip', 0.5" to read scaled images but it did not work
+                T.roi_table = ip.rois_from_imagej(values['-IJ_ROI_PATH-'], '.zip', 16 , 0.5) # Oeyvind added '.zip', 16 , 0.5 to read scaled images
             elif values['-EXISTING_ROI-']:
                 T.roi_table = ip.rois_from_csv(values['-ROI_FILE_PATH-'])
             window['-VIEW_ROI-'].update(disabled = False)
