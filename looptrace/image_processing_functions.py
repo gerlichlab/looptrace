0f3d1ece7f33c71b6dd90f490d4bca488daf0154
Kai Sandvold Beckwith <kai.beckwith@embl.de>
2021-11-20 15:33:58 +0100

    Update image_processing_functions.py

--- looptrace/image_processing_functions.py
+++ looptrace/image_processing_functions.py
@@ -81,6 +81,7 @@ def tif_store_to_dask(folder, re_search = 'P[0-9]{4}'):
         
 def nikon_tiff_to_dask(folder):
     image_sequence = tifffile.TiffSequence(folder+os.sep+'*.tiff', pattern='(Time)(\d+)_(Point)(\d+)_(ZStack)(\d+)')
+    print('Found image folder of shape', image_sequence.shape)
     with image_sequence.aszarr() as store:
         z = zarr.open(store, mode='r')
         images = da.transpose(da.from_zarr(z), (1,0,3,2,4,5))
