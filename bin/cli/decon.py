4951754301de788e697fe4c8d28d880e5baeb058
Vince Reuter <vince.reuter@gmail.com>
2023-06-15 16:28:39 +0200

    cleaner syntax for key-value retrieval, organise imports

--- bin/cli/decon.py
+++ bin/cli/decon.py
@@ -6,10 +6,10 @@ Ellenberg group
 EMBL Heidelberg
 """
 
-from looptrace.ImageHandler import ImageHandler
-from looptrace.Deconvolver import Deconvolver
 import argparse
 import os
+from looptrace.ImageHandler import ImageHandler
+from looptrace.Deconvolver import Deconvolver
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract experimental PSF from bead images.')
@@ -18,9 +18,6 @@ if __name__ == '__main__':
     parser.add_argument("--image_save_path", help="(Optional): Path to folder to save images to.", default=None)
     args = parser.parse_args()
     H = ImageHandler(config_path=args.config_path, image_path=args.image_path, image_save_path=args.image_save_path)
-    try:
-        array_id = int(os.environ["SLURM_ARRAY_TASK_ID"])
-    except KeyError:
-        array_id = None
-    Dc = Deconvolver(H, array_id = array_id)
+    array_id = os.environ.get("SLURM_ARRAY_TASK_ID")
+    Dc = Deconvolver(H, array_id = None if array_id is None else int(array_id))
     Dc.decon_seq_images()
