b134b7953a142f1934091f2cfa3fe3b7e73eea8a
Vince Reuter <vince.reuter@gmail.com>
2023-06-15 16:31:30 +0200

    dependencies and environment through Nix for deconvolution

--- shell.nix
+++ shell.nix
@@ -1,12 +1,13 @@
 {
+  deconvolution ? false,
   pkgs ? import (builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "nixos-23.05";
     rev = "4ecab3273592f27479a583fb6d975d4aba3486fe";
   }) {}
 }:
-let py310 = pkgs.python310.withPackages (ps: with ps; [ numpy pandas ]);
-    py311 = pkgs.python311.withPackages (ps: with ps; [ numpy pandas ]);
+let py310 = pkgs.python310.withPackages (ps: with ps; [ numpy pandas ] ++ (if deconvolution then [ tensorflowWithCuda ] else []));
+    py311 = pkgs.python311.withPackages (ps: with ps; [ numpy pandas ] ++ (if deconvolution then [ tensorflowWithCuda ] else []));
 in
 pkgs.mkShell {
   name = "tracebot-env";
@@ -16,7 +17,9 @@ pkgs.mkShell {
     py311
     pkgs.zlib
     pkgs.stdenv.cc.cc.lib
-  ];
+  ] ++ (
+    if deconvolution then [] else []
+  );
   shellHook = ''
     poetry env use "${py310}/bin/python"
     export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib"
