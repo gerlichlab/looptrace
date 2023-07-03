1ac9311d0c50c28319bfda4e02aea9e40f57c891
Vince Reuter <vince.reuter@gmail.com>
2023-07-03 11:21:19 +0200

    better handling of argument typing

--- bin/cli/detect_spots_preview.py
+++ bin/cli/detect_spots_preview.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preview spot detection in given position.')
     parser.add_argument("config_path", help="Config file path")
     parser.add_argument("image_path", help="Path to folder with images to read.")
-    parser.add_argument('--position', help='(Optional): Index of position to view.', default='0')
+    parser.add_argument('--position', type=int, help='(Optional): Index of position to view.', default=0)
 
     args = parser.parse_args()
     H = ImageHandler(config_path=args.config_path, image_path=args.image_path)
     S = SpotPicker(H)
-    preview_pos = H.image_lists[H.config['spot_input_name']][int(args.position)]
+    preview_pos = H.image_lists[H.config['spot_input_name']][args.position]
     S.rois_from_spots(preview_pos=preview_pos)
