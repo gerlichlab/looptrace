1bacca43342918886b4e494ab4c09b21e4f2284a
Vince Reuter <vince.reuter@gmail.com>
2023-07-05 14:02:21 +0200

    use updated tracing analysis function utilities

--- looptrace/trace_analysis_functions.py
+++ looptrace/trace_analysis_functions.py
@@ -22,6 +22,7 @@ import os
 import itertools
 import logging
 from math import cos, sin, radians
+from pathlib import Path
 import re
 from typing import *
 import warnings
@@ -41,8 +42,6 @@ from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tqdm
-
-from .pathtools import ExtantFile
 
 warnings.simplefilter('ignore', category=NumbaPerformanceWarning)
 
@@ -129,27 +128,34 @@ def pylochrom_coords_to_traces(coords):
     return pd.concat(traces)
 
 
-def apply_frame_names(traces_file: ExtantFile, frame_names: Sequence[str]) -> pd.DataFrame:
+def read_traces_and_apply_frame_names(traces_file: Path, frame_names: Sequence[str]) -> pd.DataFrame:
     logger.debug(f"Reading traces: {traces_file}")
-    traces = pd.read_csv(traces_file.path, index_col=0)
+    traces = pd.read_csv(traces_file, index_col=0)
     logger.debug(f"Applying frame names to traces")
     traces[FRAME_NAMES_KEY] = traces.apply(lambda row: frame_names[row[FRAME_INDEX_KEY]], axis=1)
     return traces
 
 
-def apply_traces_qc(traces_file: ExtantFile, config_file: ExtantFile, min_trace_length: int, exclusions: Optional[Iterable[str]]) -> pd.DataFrame:
+def read_traces_parse_frame_names_and_apply_traces_qc(
+        traces_file: Path, 
+        config_file: Path, 
+        min_trace_length: Optional[int], 
+        exclusions: Optional[Iterable[str]],
+        ) -> Tuple[List[str], pd.DataFrame]:
     import yaml
     logger.debug(f"Reading config file: {config_file}")
-    with open(config_file.path, 'r') as fh:
+    with open(config_file, 'r') as fh:
         config = yaml.safe_load(fh)
     frame_names = config[FRAME_NAMES_KEY]
     logger.debug(f"{len(frame_names)} frame names: {', '.join(frame_names)}")
-    traces = apply_frame_names(traces_file=traces_file, frame_names=frame_names)
+    traces = read_traces_and_apply_frame_names(traces_file=traces_file, frame_names=frame_names)
     logger.info("Applying general trace QC")
     traces['QC'] = tracing_qc(traces, config).astype(int)
     logger.info("Applying trace length QC")
-    traces = tracing_length_qc(traces[~traces[FRAME_NAMES_KEY].isin(exclusions)] if exclusions else traces, min_length=min_trace_length)
-    return traces
+    filter_by_name = lambda df: df[~df[FRAME_NAMES_KEY].isin(exclusions)] if exclusions else df
+    filter_by_length = lambda df: tracing_length_qc(traces=df, min_length=min_trace_length) if min_trace_length else df
+    traces = filter_by_length(filter_by_name(traces))
+    return frame_names, traces
 
 
 def tracing_qc(traces, qc_config):
@@ -363,22 +369,10 @@ def pwd_calc(traces):
     pwds = np.stack(pwds)
     return pwds
 
-def tracing_length_qc(traces, min_length=0):
-    '''
-    Parameters
-    ----------
-    traces : pd DataFrame with trace data.
-    min_length : Int, minimum length of trace to pass QC
+def tracing_length_qc(traces: pd.DataFrame, min_length: int = 0) -> pd.DataFrame:
+    '''Remove traces that are not sufficiently long.'''
+    return traces.groupby('trace_id').filter(lambda x: x['QC'].sum() >= min_length)
 
-    Returns
-    -------
-    traces : pd DataFrame with shorter traces removed.
-    #pwds : 3-dim np array of pwds that passed length QC.
-
-    '''
-    grouped=traces.groupby('trace_id')
-    traces_long=grouped.filter(lambda x : x['QC'].sum()>=min_length)
-    return traces_long
 
 def trace_analysis(traces, pwds):
     '''
