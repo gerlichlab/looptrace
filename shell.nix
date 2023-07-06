1e64be300f5ed637c7807f6fe63c9b1cd78271cb
Vince Reuter <vince.reuter@gmail.com>
2023-07-06 16:12:39 +0200

    better handling of dependency groups for nix shell

--- shell.nix
+++ shell.nix
@@ -4,19 +4,27 @@
     ref = "nixos-23.05";
     rev = "4ecab3273592f27479a583fb6d975d4aba3486fe";
   }) {}, 
-  pipeline ? true,
-  analysis ? false
+  pipeline ? false,
+  analysis ? false, 
+  pydev ? false,
 }:
-let baseBuildInputs = with pkgs; [ zlib stdenv.cc.cc.lib ];
+let baseBuildInputs = with pkgs; [ poetry stdenv.cc.cc.lib zlib ];
     py310 = pkgs.python310.withPackages (ps: with ps; [ numpy pandas ]);
     py311 = pkgs.python311.withPackages (ps: with ps; [ numpy pandas ]);
     R-analysis = pkgs.rWrapper.override{ packages = with pkgs.rPackages; [ data_table ggplot2 ]; };
+    poetryGroups = [] ++ 
+      (if pipeline then ["pipeline"] else []) ++
+      (if analysis then ["analysis"] else []) ++ 
+      (if pydev then ["dev"] else []);
+    poetryInstallExtras = (
+      if poetryGroups == [] then ""
+      else " --with " ++ pkgs.lib.concatStringsSep "," poetryGroups
+      );
 in
 pkgs.mkShell {
   name = "looptrace-env";
   buildInputs = baseBuildInputs ++ 
     (if pipeline then [
-      pkgs.poetry
       py310
       py311
       pkgs.zlib
@@ -26,7 +34,7 @@ pkgs.mkShell {
   shellHook = ''
     poetry env use "${py310}/bin/python"
     export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib"
-    poetry install --sync --with=dev
+    poetry install --sync${poetryInstallExtras}
     source "$(poetry env info --path)/bin/activate"
   '';
 }
