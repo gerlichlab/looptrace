bd705df47402c2fe1ec2eacaa62657ed30a37608
Vince <vince.reuter@gmail.com>
2023-08-03 15:53:09 +0200

    Check for logging in main pipeline (#5)
    * first testing of main pipeline script
    
    * use new version of gertils
    
    * first tests in place and passing for main pipeline CLI optargs requirements
    
    * helper function for pipeline construction
    
    * hook for interactive / no-tee pipeline manager
    
    * pass logging tests for main pipeline, close #49; remove logmuse from main pipeline

--- bin/cli/run_processing_pipeline.py
+++ bin/cli/run_processing_pipeline.py
@@ -1,11 +1,11 @@
 """Simple end-to-end processing pipeline for chromatin tracing data"""
 
 import argparse
+import logging
 import sys
 from typing import *
 
 from gertils.pathtools import ExtantFile, ExtantFolder
-import logmuse
 import pypiper
 
 from extract_exp_psf import workflow as run_psf_extraction
@@ -20,17 +20,18 @@ from extract_spots_table import workflow as run_spot_bounding
 from extract_spots import workflow as run_spot_extraction
 from tracing import workflow as run_chromatin_tracing
 
-logger = None
+logger = logging.getLogger(__name__)
 
+NO_TEE_LOGS_OPTNAME = "--do-not-tee-logs"
 PIPE_NAME = "looptrace"
 
 
 class LooptracePipeline(pypiper.Pipeline):
 
-    def __init__(self, config_file: ExtantFile, images_folder: ExtantFolder, output_folder: ExtantFolder) -> None:
+    def __init__(self, config_file: ExtantFile, images_folder: ExtantFolder, output_folder: ExtantFolder, **pl_mgr_kwargs: Any) -> None:
         self.config_file = config_file
         self.images_folder = images_folder
-        super(LooptracePipeline, self).__init__(name=PIPE_NAME, outfolder=str(output_folder.path))
+        super(LooptracePipeline, self).__init__(name=PIPE_NAME, outfolder=str(output_folder.path), **pl_mgr_kwargs)
 
     def stages(self) -> List[Callable]:
         conf_data_pair = (self.config_file, self.images_folder)
@@ -54,25 +55,31 @@ class LooptracePipeline(pypiper.Pipeline):
         return [pypiper.Stage(func=fxn, f_args=fxn_args, name=name) for name, fxn, fxn_args in func_args_pairs]
 
 
-def parse_cmdl(cmdl: argparse.Namespace):
+def parse_cli(args: Iterable[str]) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="A pipeline to process microscopy imaging data to trace chromatin fiber with FISH probes")
     parser.add_argument("-C", "--config-file", type=ExtantFile.from_string, required=True, help="Path to the main configuration file")
     parser.add_argument("-I", "--images-folder", type=ExtantFolder.from_string, required=True, help="Path to the root folder with imaging data to process")
     parser.add_argument("-O", "--output-folder", type=ExtantFolder.from_string, required=True, help="Path to folder for pypiper output")
+    parser.add_argument(NO_TEE_LOGS_OPTNAME, action="store_true", help="Do not tee logging output from pypiper manager")
     parser = pypiper.add_pypiper_args(
         parser, 
         groups=("pypiper", "checkpoint"), # This adds logmuse options, too (from pypiper group).
         args=("start-point", ),
         )
-    return parser.parse_args(cmdl)
+    return parser.parse_args(args)
+
+
+def init(opts: argparse.Namespace) -> LooptracePipeline:
+    kwargs = {"config_file": opts.config_file, "images_folder": opts.images_folder, "output_folder": opts.output_folder}
+    if opts.do_not_tee_logs:
+        kwargs["multi"] = True
+    logger.info(f"Building {PIPE_NAME} pipeline from {opts.config_file.path}, to use images from {opts.images_folder.path}")
+    return LooptracePipeline(**kwargs)
 
 
 def main(cmdl):
-    opts = parse_cmdl(cmdl)
-    global logger
-    logger = logmuse.logger_via_cli(opts)
-    logger.info(f"Building {PIPE_NAME} pipeline from {opts.config_file}, to use images from {opts.images_folder}")
-    pipeline = LooptracePipeline(config_file=opts.config_file, images_folder=opts.images_folder, output_folder=opts.output_folder)
+    opts = parse_cli(cmdl)
+    pipeline = init(opts)
     logger.info("Running pipeline")
     pipeline.run(start_point=opts.start_point, stop_after=opts.stop_after)
 
---
+++ tests/test_pipeline.py
@@ -0,0 +1,160 @@
+"""Tests for the main processing pipeline"""
+
+from dataclasses import dataclass
+import itertools
+import logging
+from pathlib import Path
+from typing import *
+
+import pytest
+import yaml
+from gertils import PathWrapperException
+
+from conftest import import_pipeline_script
+
+looptrace_pipeline = import_pipeline_script()
+
+
+@dataclass
+class CliOptProvision:
+    optname: Optional[str]
+    get_path: Callable[[Path], Path]
+    prepare_path: Optional[Callable[[Path], Optional[Path]]]
+
+    @property
+    def is_legitimate(self) -> bool:
+        return self.is_specified and self.is_prepared
+
+    @property
+    def is_prepared(self) ->  bool:
+        return self.prepare_path is not None
+
+    @property
+    def is_specified(self) -> bool:
+        return self.optname is not None
+    
+    def __repr__(self) -> str:
+        return f"(optname={self.optname}, prepared={self.is_prepared})"
+
+    def __str__(self) -> str:
+        return f"(optname={self.optname}, prepared={self.is_prepared})"
+
+
+@dataclass
+class CliSpecMinimal:
+    config_file: CliOptProvision
+    images_folder: CliOptProvision
+    output_folder: CliOptProvision
+
+    @property
+    def is_legitimate(self) -> bool:
+        return self.config_file.is_legitimate and self.images_folder.is_legitimate and self.output_folder.is_legitimate
+
+    def prepare_list(self, folder: Path) -> List[str]:
+        result = []
+        for o in (self.config_file, self.images_folder, self.output_folder):
+            if o.optname is None:
+                continue
+            path = o.get_path(folder)
+            if o.prepare_path is not None:
+                o.prepare_path(path)
+            result.extend([o.optname, str(path)])
+        return result
+    
+    def __repr__(self) -> str:
+        return repr((self.config_file, self.images_folder, self.output_folder))
+
+    def __str__(self) -> str:
+        return str((self.config_file, self.images_folder, self.output_folder))
+
+
+def prep_config_file(folder: Path, create: bool) -> Path:
+    fp = folder / "config.yaml"
+    if create:
+        fp.write_text("", encoding='utf-8')
+    assert (create and fp.is_file()) or (not create and not fp.is_file())
+    return fp
+
+
+def _prep_subfolder(folder: Path, name: str, create: bool) -> Path:
+    fp = folder / name
+    if create:
+        fp.mkdir()
+    assert (create and fp.is_dir()) or (not create and not fp.is_dir())
+    return fp
+
+
+def prep_images_folder(folder: Path, create: bool) -> Path:
+    return _prep_subfolder(folder=folder, name="images", create=create)
+
+
+def prep_output_folder(folder: Path, create: bool) -> Path:
+    return _prep_subfolder(folder=folder, name="output", create=create)
+
+
+CONFIG_FILE_SPECS = [
+    CliOptProvision(opt, lambda p: p / "config.yaml", (lambda p: p.write_text("", encoding='utf-8')) if create else None) 
+    for opt, create in itertools.product((None, "-C", "--config-file"), (False, True))
+    ]
+IMAGE_FOLDER_SPECS = [
+    CliOptProvision(opt, lambda p: p / "images", (lambda p: p.mkdir()) if create else None) 
+    for opt, create in itertools.product((None, "-I", "--images-folder"), (False, True))
+    ]
+OUTPUT_FOLDER_SPECS = [
+    CliOptProvision(opt, lambda p: p / "output", (lambda p: p.mkdir()) if create else None) 
+    for opt, create in itertools.product((None, "-O", "--output-folder"), (False, True))]
+
+
+COMMAND_LINES = [CliSpecMinimal(config_file=conf, images_folder=imgs, output_folder=out) for conf, imgs, out in itertools.product(CONFIG_FILE_SPECS, IMAGE_FOLDER_SPECS, OUTPUT_FOLDER_SPECS)]
+
+
+@pytest.mark.parametrize(["cli", "expect_success"], [(cli, cli.is_legitimate) for cli in COMMAND_LINES], ids=str)
+def test_required_inputs(tmp_path, cli, expect_success):
+    args = cli.prepare_list(tmp_path)
+    print(f"ARGS: {args}")
+    try:
+        looptrace_pipeline.parse_cli(args)
+    except (PathWrapperException, SystemExit) as error:
+        if expect_success:
+            pytest.fail(f"Expected success but got error -- {error}")
+        else:
+            if isinstance(error, SystemExit):
+                assert 2 == error.code
+    else:
+        if expect_success:
+            pass
+        else:
+            pytest.fail("When testing, unexpected success is a failure!")
+
+
+@pytest.fixture
+def prepped_minimal_config_data(tmp_path):
+    analysis_path = tmp_path / "analysis"
+    analysis_path.mkdir()
+    return {
+        "analysis_path": str(analysis_path),
+        "analysis_prefix": "TESTING__",
+        "decon_input_name": "seq_images_raw_decon"
+    }
+
+
+@pytest.mark.parametrize("config_file_option", ["-C", "--config-file"])
+@pytest.mark.parametrize("images_folder_option", ["-I", "--images-folder"])
+@pytest.mark.parametrize("output_folder_option", ["-O", "--output-folder"])
+def test_logging(tmp_path, prepped_minimal_config_data, caplog, config_file_option, images_folder_option, output_folder_option):
+    caplog.set_level(logging.INFO)
+    conf_path = tmp_path / "config.yaml"
+    with open(conf_path, 'w') as fh:
+        yaml.dump(prepped_minimal_config_data, fh)
+    imgs_path = prep_images_folder(tmp_path, create=True)
+    output_folder = prep_output_folder(tmp_path, create=True)
+    opts = looptrace_pipeline.parse_cli([
+        config_file_option, str(conf_path), 
+        images_folder_option, str(imgs_path), 
+        output_folder_option, str(output_folder), 
+        looptrace_pipeline.NO_TEE_LOGS_OPTNAME,
+        ])
+    # Add a .stop_pipeline() call here to avoid (non-fatal) errors from logging system.
+    # See: https://github.com/databio/pypiper/issues/186
+    looptrace_pipeline.init(opts).manager.stop_pipeline()
+    assert f"Building {looptrace_pipeline.PIPE_NAME} pipeline from {conf_path}, to use images from {imgs_path}" in caplog.text
