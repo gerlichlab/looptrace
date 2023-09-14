f09928291c837daf8652dec0f51bf2524b15a37b
Vince Reuter <vince.reuter@gmail.com>
2023-09-14 15:04:47 +0200

    move DC accuracy analysis to end of pipeline; close #67

--- bin/cli/run_processing_pipeline.py
+++ bin/cli/run_processing_pipeline.py
@@ -42,8 +42,6 @@ class LooptracePipeline(pypiper.Pipeline):
             ("deconvolution", run_deconvolution, conf_data_pair), 
             ("nuclei_detection", run_nuclei_detection, conf_data_pair), 
             ("drift_correction", run_drift_correction, conf_data_pair), 
-            ("drift_correction_accuracy_analysis", run_drift_correction_analysis, conf_data_pair), 
-            ("drift_correction_accuracy_visualisation", run_drift_correction_accuracy_visualisation, conf_only), 
             ("spot_detection", run_spot_detection, conf_data_pair), # generates *_rois.csv (regional spots)
             ("spot_filtration", run_spot_filtration, conf_data_pair), 
             ("clean_1", run_cleanup, conf_only),
@@ -53,6 +51,9 @@ class LooptracePipeline(pypiper.Pipeline):
             ("clean_2", run_cleanup, conf_only), 
             ("tracing", run_chromatin_tracing, conf_data_pair),
             ("clean_3", run_cleanup, conf_only),
+            ("drift_correction_accuracy_analysis", run_drift_correction_analysis, conf_data_pair), 
+            ("drift_correction_accuracy_visualisation", run_drift_correction_accuracy_visualisation, conf_only), 
+            
         )
         return [pypiper.Stage(func=fxn, f_args=fxn_args, name=name) for name, fxn, fxn_args in func_args_pairs]
 
