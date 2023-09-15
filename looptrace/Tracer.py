87ed139a5db09e9cbc03334151e054e8f8359c3b
Vince <vince.reuter@gmail.com>
2023-09-15 10:25:17 +0200

    Vr/tracing qc 65 (#70)
    * cleaner syntax
    
    * cleaner handling of beads tracing case
    
    * include tracing QC in main pipeline; deprecate old QC; close #65

--- looptrace/Tracer.py
+++ looptrace/Tracer.py
@@ -18,23 +18,23 @@ ROI_FIT_COLUMNS = ["BG", "A", "z_px", "y_px", "x_px", "sigma_z", "sigma_xy"]
 
 
 class Tracer:
-    def __init__(self, image_handler, trace_beads = False, array_id = None):
+
+    def __init__(self, image_handler, trace_beads=False, array_id=None):
         '''
         Initialize Tracer class with config read in from YAML file.
-    '''
+        '''
         self.image_handler = image_handler
         self.config_path = image_handler.config_path
         self.config = image_handler.config
         self.drift_table = image_handler.tables[image_handler.spot_input_name + '_drift_correction']
         self.images = self.image_handler.images[self.config['trace_input_name']]
-            
         self.pos_list = self.image_handler.image_lists[image_handler.spot_input_name]
-        self.traces_path = self.image_handler.out_path('traces.csv')
-        self.trace_beads = trace_beads
         if trace_beads:
             self.roi_table = image_handler.tables[image_handler.spot_input_name + '_bead_rois']
+            finalise_suffix = lambda p: p.replace(".csv", "_beads.csv")
         else:
             self.roi_table = image_handler.tables[image_handler.spot_input_name + '_rois']
+            finalise_suffix = lambda p: p
         self.all_rois = image_handler.tables[image_handler.spot_input_name + '_dc_rois']
 
         self.fit_funcs = {'LS': fitSymmetricGaussian3D, 'MLE': fitSymmetricGaussian3DMLE}
@@ -45,9 +45,12 @@ class Tracer:
             self.pos_list = [self.pos_list[int(self.array_id)]]
             self.roi_table = self.roi_table[self.roi_table.position.isin(self.pos_list)]
             self.all_rois = self.all_rois[self.all_rois.position.isin(self.pos_list)].reset_index(drop=True)
-            self.traces_path = self.image_handler.out_path('traces.csv'[:-4] + '_' + str(self.array_id).zfill(4) + '.csv')
+            traces_path = self.image_handler.out_path('traces.csv'[:-4] + '_' + str(self.array_id).zfill(4) + '.csv')
             self.images = self.images[self.roi_table.index.to_list()]
-
+        else:
+            traces_path = self.image_handler.out_path('traces.csv')
+        
+        self.traces_path = finalise_suffix(traces_path)
 
     def trace_single_roi(self, roi_img, mask = None, background = None):
         #Fit a single roi with 3D gaussian (MLE or LS as defined in config).
@@ -63,8 +66,7 @@ class Tracer:
             return self.fit_func(roi_img, sigma=1, center=center)[0]
         else:
             return np.array([-1] * len(ROI_FIT_COLUMNS))
-        
-
+    
     def trace_all_rois(self) -> str:
         '''
         Fits 3D gaussian to previously detected ROIs across positions and timeframes.
@@ -107,23 +109,20 @@ class Tracer:
         traces.rename(columns={"roi_id": "trace_id"}, inplace=True)
 
         #Apply fine scale drift to fits, and physcial units.
-        traces['z_px_dc']=traces['z_px']+traces['z_px_fine']
-        traces['y_px_dc']=traces['y_px']+traces['y_px_fine']
-        traces['x_px_dc']=traces['x_px']+traces['x_px_fine']
+        traces['z_px_dc'] = traces['z_px'] + traces['z_px_fine']
+        traces['y_px_dc'] = traces['y_px'] + traces['y_px_fine']
+        traces['x_px_dc'] = traces['x_px'] + traces['x_px_fine']
         #traces=traces.drop(columns=['drift_z', 'drift_y', 'drift_x'])
-        traces['z']=traces['z_px_dc']*self.config['z_nm']
-        traces['y']=traces['y_px_dc']*self.config['xy_nm']
-        traces['x']=traces['x_px_dc']*self.config['xy_nm']
-        traces['sigma_z']=traces['sigma_z']*self.config['z_nm']
-        traces['sigma_xy']=traces['sigma_xy']*self.config['xy_nm']
+        traces['z'] = traces['z_px_dc'] * self.config['z_nm']
+        traces['y'] = traces['y_px_dc'] * self.config['xy_nm']
+        traces['x'] = traces['x_px_dc'] * self.config['xy_nm']
+        traces['sigma_z'] = traces['sigma_z'] * self.config['z_nm']
+        traces['sigma_xy'] = traces['sigma_xy'] * self.config['xy_nm']
         traces = traces.sort_values(['trace_id', 'frame'])
 
-        if self.trace_beads: 
-            suffix = '_beads.csv'
-        else:
-            suffix = ''
-
-        #self.image_handler.traces = traces
-        outfile = self.traces_path + suffix
+        outfile = self.finalise_suffix(self.traces_path)
+        
+        print(f"Writing traces: {outfile}")
         traces.to_csv(outfile)
+
         return outfile
