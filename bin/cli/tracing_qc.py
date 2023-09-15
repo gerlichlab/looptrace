ca6e117e42a6818b019c6e3553a96f9b619a7a69
Vince Reuter <vince.reuter@gmail.com>
2023-09-16 00:12:24 +0200

    fix data type of config data

--- bin/cli/tracing_qc.py
+++ bin/cli/tracing_qc.py
@@ -182,9 +182,10 @@ def workflow(config_file: ExtantFile, images_folder: ExtantFolder) -> Tuple[Path
     array_id = os.environ.get("SLURM_ARRAY_TASK_ID")
     tracer = Tracer(image_handler=image_handler, array_id=None if array_id is None else int(array_id))
     traces_file = Path(tracer.traces_path)
-    with open(config_file.path, 'r') as conf:
-        probes_to_ignore = conf["illegal_frames_for_trace_support"]
-        min_trace_length = conf.get("min_trace_length", 0)
+    with open(config_file.path, 'r') as tmp_cfg:
+        conf_data = yaml.safe_load(tmp_cfg)
+    probes_to_ignore = conf_data["illegal_frames_for_trace_support"]
+    min_trace_length = conf_data.get("min_trace_length", 0)
     print(f"Probes to ignore: {', '.join(probes_to_ignore)}")
     print(f"Min trace length: {min_trace_length}")
     return apply_qc_filtration_and_write_results(traces_file=traces_file, config_file=config_file.path, min_trace_length=min_trace_length, exclusions=probes_to_ignore)
