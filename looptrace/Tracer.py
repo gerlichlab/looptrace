b776d9a9e0a329f8282025f02d0383049fd9c277
Vince Reuter <vince.reuter@gmail.com>
2023-09-19 16:25:54 +0200

    split apart the tracing functionality to separate concerns and isolate functinality for testability and easier comprehension

--- looptrace/Tracer.py
+++ looptrace/Tracer.py
@@ -64,75 +64,94 @@ class Tracer:
         
         self.traces_path = finalise_suffix(traces_path)
 
-    def trace_single_roi(self, roi_img, mask = None, background = None):
-        #Fit a single roi with 3D gaussian (MLE or LS as defined in config).
-        #Masking by intensity or label image can be used to improve fitting correct spot (set in config)
-        if background is not None:
-            roi_img = roi_img - background
-        if np.any(roi_img) and np.all([d > 2 for d in roi_img.shape]): #Check if empty or too small for fitting
-            if mask is None:
-                center = 'max'
-            else:
-                roi_img_masked = roi_img * (mask / np.max(mask))**2
-                center = list(np.unravel_index(np.argmax(roi_img_masked, axis=None), roi_img.shape))
-            return self.fit_func(roi_img, sigma=1, center=center)[0]
-        else:
-            return np.array([-1] * len(ROI_FIT_COLUMNS))
-    
     def trace_all_rois(self) -> str:
         '''
         Fits 3D gaussian to previously detected ROIs across positions and timeframes.
     
         '''
-        imgs = self.images
-
-        fits = []
-
         #fits = Parallel(n_jobs=-1, prefer='threads')(delayed(self.trace_single_roi)(roi_imgs[i]) for i in tqdm(range(roi_imgs.shape[0])))
-        mask_fits = self.image_handler.config.get('mask_fits', False)
         
         try:
             #This only works for a single position at the time currently
-            background = self.image_handler.config['subtract_background']
-            pos_drifts = self.drift_table[self.drift_table.position.isin(self.pos_list)][['z_px_fine', 'y_px_fine', 'x_px_fine']].to_numpy()
-            background_rel_drifts = pos_drifts - pos_drifts[background]
+            bg_frame_idx = self.image_handler.config['subtract_background']
         except KeyError:
-            background = None
-
-        if mask_fits:
-            ref_frames = self.roi_table['frame'].to_list()
-            for p, pos_imgs in tqdm(enumerate(imgs), total=len(imgs)):
-                ref_img = pos_imgs[ref_frames[p]]
-                #print(ref_img.shape)
-                for t, spot_img in enumerate(pos_imgs):
-                    if background is not None:
-                        spot_img = np.clip(spot_img.astype(np.int16) - ndi.shift(pos_imgs[background], shift = background_rel_drifts[t]), a_min = 0, a_max = None)
-                    fits.append(self.trace_single_roi(spot_img, mask = ref_img))
-                #Parallel(n_jobs=1, prefer='threads')(delayed(self.trace_single_roi)(imgs[p, t], mask= ref_img) for t in range(imgs.shape[1]))
-
+            bg_spec = None
         else:
-            for pos_imgs in tqdm(imgs, total=len(imgs)):
-                for spot_img in pos_imgs:
-                    fits.append(self.trace_single_roi(spot_img))
+            pos_drifts = self.drift_table[self.drift_table.position.isin(self.pos_list)][['z_px_fine', 'y_px_fine', 'x_px_fine']].to_numpy()
+            bg_spec = BackgroundSpecification(frame_index=bg_frame_idx, drifts=pos_drifts - pos_drifts[bg_frame_idx])
 
-        trace_res = pd.DataFrame(fits,columns=ROI_FIT_COLUMNS)
+        trace_res = find_trace_fits(
+            fit_func=self.fit_func,
+            images=self.images, 
+            ref_frames=self.roi_table['frame'].to_list(), 
+            mask_fits=self.image_handler.config.get('mask_fits', False), 
+            background_specification=bg_spec
+            )
+
         #trace_index = pd.DataFrame(fit_rois, columns=["trace_id", "frame", "ref_frame", "position", "drift_z", "drift_y", "drift_x"])
         traces = pd.concat([self.all_rois, trace_res], axis=1)
         traces.rename(columns={"roi_id": "trace_id"}, inplace=True)
 
         #Apply fine scale drift to fits, and physcial units.
-        traces['z_px_dc'] = traces['z_px'] + traces['z_px_fine']
-        traces['y_px_dc'] = traces['y_px'] + traces['y_px_fine']
-        traces['x_px_dc'] = traces['x_px'] + traces['x_px_fine']
+        traces = apply_fine_scale_drift_correction(traces)
         #traces=traces.drop(columns=['drift_z', 'drift_y', 'drift_x'])
-        traces['z'] = traces['z_px_dc'] * self.config['z_nm']
-        traces['y'] = traces['y_px_dc'] * self.config['xy_nm']
-        traces['x'] = traces['x_px_dc'] * self.config['xy_nm']
-        traces['sigma_z'] = traces['sigma_z'] * self.config['z_nm']
-        traces['sigma_xy'] = traces['sigma_xy'] * self.config['xy_nm']
-        traces = traces.sort_values(['trace_id', 'frame'])
+        traces = apply_pixels_to_nanometers(traces, z_nm_per_px=self.config['z_nm'], xy_nm_per_px=self.config['xy_nm'])
         
+        traces = traces.sort_values(['trace_id', 'frame'])
         print(f"Writing traces: {self.traces_path}")
         traces.to_csv(self.traces_path)
 
         return self.traces_path
+    
+
+def find_trace_fits(fit_func, images: Iterable[np.ndarray], ref_frames: List[int], mask_fits: bool, background_specification: Optional[BackgroundSpecification]) -> pd.DataFrame:
+    fits = []
+    if mask_fits:
+        for p, pos_imgs in tqdm(enumerate(images), total=len(images)):
+            ref_img = pos_imgs[ref_frames[p]]
+            #print(ref_img.shape)
+            for t, spot_img in enumerate(pos_imgs):
+                if background_specification is not None:
+                    shift = ndi.shift(pos_imgs[background_specification.frame_index], shift=background_specification.drifts[t])
+                    spot_img = np.clip(spot_img.astype(np.int16) - shift, a_min = 0, a_max = None)
+                fits.append(trace_single_roi(fit_func=fit_func, roi_img=spot_img, mask=ref_img))
+            #Parallel(n_jobs=1, prefer='threads')(delayed(self.trace_single_roi)(imgs[p, t], mask= ref_img) for t in range(imgs.shape[1]))
+    else:
+        for pos_imgs in tqdm(images, total=len(images)):
+            for spot_img in pos_imgs:
+                fits.append(trace_single_roi(fit_func=fit_func, roi_img=spot_img))
+    return pd.DataFrame(fits, columns=ROI_FIT_COLUMNS)
+
+
+def trace_single_roi(fit_func, roi_img, mask=None, background=None):
+    #Fit a single roi with 3D gaussian (MLE or LS as defined in config).
+    #Masking by intensity or label image can be used to improve fitting correct spot (set in config)
+    if background is not None:
+        roi_img = roi_img - background
+    if np.any(roi_img) and np.all([d > 2 for d in roi_img.shape]): #Check if empty or too small for fitting
+        if mask is None:
+            center = 'max'
+        else:
+            roi_img_masked = roi_img * (mask / np.max(mask))**2
+            center = list(np.unravel_index(np.argmax(roi_img_masked, axis=None), roi_img.shape))
+        return fit_func(roi_img, sigma=1, center=center)[0]
+    else:
+        return np.array([-1] * len(ROI_FIT_COLUMNS))
+
+
+def apply_fine_scale_drift_correction(traces: pd.DataFrame) -> pd.DataFrame:
+    """Shift pixel coordinates by the amount of fine-scale drift correction."""
+    traces['z_px_dc'] = traces['z_px'] + traces['z_px_fine']
+    traces['y_px_dc'] = traces['y_px'] + traces['y_px_fine']
+    traces['x_px_dc'] = traces['x_px'] + traces['x_px_fine']
+    return traces
+
+
+def apply_pixels_to_nanometers(traces: pd.DataFrame, z_nm_per_px: float, xy_nm_per_px: float) -> pd.DataFrame:
+    """Add columns for distance in nanometers, based on pixel-to-nanometer conversions."""
+    traces['z'] = traces['z_px_dc'] * z_nm_per_px
+    traces['y'] = traces['y_px_dc'] * xy_nm_per_px
+    traces['x'] = traces['x_px_dc'] * xy_nm_per_px
+    traces['sigma_z'] = traces['sigma_z'] * z_nm_per_px
+    traces['sigma_xy'] = traces['sigma_xy'] * xy_nm_per_px
+    return traces
