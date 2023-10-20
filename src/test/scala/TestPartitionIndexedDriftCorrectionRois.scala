7e33f80124b9ce5edf68d7a2d7c8138cf230e86a
Vince Reuter <vince.reuter@gmail.com>
2023-10-20 18:09:54 +0200

    first 2 tests for rountrip of SelectedRoi JSON

--- src/test/scala/TestPartitionIndexedDriftCorrectionRois.scala
+++ src/test/scala/TestPartitionIndexedDriftCorrectionRois.scala
@@ -1,5 +1,30 @@
 package at.ac.oeaw.imba.gerlich.looptrace
 
-class TestPartitionIndexedDriftCorrectionRois {
-    
+import upickle.default.*
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.*
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+import at.ac.oeaw.imba.gerlich.looptrace.space.CoordinateSequence
+
+/** Tests for the partitioning of regions of interest (ROIs) for drift correction */
+class TestPartitionIndexedDriftCorrectionRois extends AnyFunSuite with ScalaCheckPropertyChecks with should.Matchers with LooptraceSuite {
+    import SelectedRoi.*
+
+    test("ROI for shifting roundtrips through JSON") {
+        forAll { (original: RoiForShifting, coordseq: CoordinateSequence) =>
+            given rw: ReadWriter[RoiForShifting] = SelectedRoi.simpleShiftingRW(coordseq)
+            val json = write(original)
+            original shouldEqual read[RoiForShifting](json)
+        }
+    }
+
+    test("ROI for accuracy roundtrips through JSON") {
+        forAll { (original: RoiForAccuracy, coordseq: CoordinateSequence) =>
+            given rw: ReadWriter[RoiForAccuracy] = SelectedRoi.simpleAccuracyRW(coordseq)
+            val json = write(original)
+            original shouldEqual read[RoiForAccuracy](json)
+        }
+    }
 }
