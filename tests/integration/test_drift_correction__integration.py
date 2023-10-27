fb0a9202ff655064634a2507a0691086151f85cb
Vince Reuter <vince.reuter@gmail.com>
2023-10-27 14:13:21 +0200

    slow tests with pytest-skip-slow; tests working with looptrace-microtest

---
+++ tests/integration/test_drift_correction__integration.py
@@ -0,0 +1,10 @@
+"""Integration tests for drift correction"""
+
+import pytest
+
+__author__ = "Vince Reuter"
+
+
+@pytest.mark.skip("not implemented")
+def test_drift_correction__reference_frame_has_no_shift_whatsoever__issue_103():
+    pass
