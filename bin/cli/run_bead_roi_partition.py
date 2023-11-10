7f77bd52f88832d4054c4211a0854c04db0b53ee
Vince Reuter <vince.reuter@gmail.com>
2023-11-10 01:50:48 +0100

    hook bead ROI partition into pipeline; #101, #108

---
+++ bin/cli/run_bead_roi_partition.py
@@ -0,0 +1,49 @@
+"""Bead ROI partitioning"""
+
+import argparse
+from pathlib import Path
+import subprocess
+from typing import *
+
+from gertils import ExtantFile, ExtantFolder
+from looptrace import LOOPTRACE_JAR_PATH
+from looptrace.ImageHandler import handler_from_cli
+
+__author__ = "Vince Reuter"
+
+
+
+def workflow(
+    config_file: ExtantFile, 
+    images_folder: ExtantFolder, 
+    output_folder: Union[None, Path, ExtantFolder] = None,
+    ):
+    H = handler_from_cli(config_file=config_file, images_folder=images_folder)
+    prog_path = "at.ac.oeaw.imba.gerlich.looptrace.PartitionDriftCorrectionRois"
+    num_del = H.num_bead_rois_for_drift_correction
+    num_acc = H.num_bead_rois_for_drift_correction_accuracy
+    output_folder = output_folder or H.bead_rois_path
+    cmd_parts = [
+        "java", 
+        "-cp",
+        LOOPTRACE_JAR_PATH,
+        prog_path, 
+        "--beadRoisRoot",
+        H.bead_rois_path, 
+        "--numShifting", 
+        str(num_del),
+        "--numAccuracy",
+        str(num_acc),
+        "--outputFolder",
+        str(output_folder)
+    ]
+    print(f"Running bead ROI partitioning: {' '.join(cmd_parts)}")
+    subprocess.check_call(cmd_parts)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Driver for computing all fiducial bead ROIs for a particular imaging experiment")
+    parser.add_argument("config_path", type=ExtantFile.from_string, help="Config file path")
+    parser.add_argument("image_path", type=ExtantFolder.from_string, help="Path to folder with images to read.")
+    parser.add_argument("-O", "--output-folder", type=Path, help="Path to folder in which to place output")
+    
