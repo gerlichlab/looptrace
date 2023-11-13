a3f0a6117534c467f88d8371efa98602b2b78248
Vince Reuter <vince.reuter@gmail.com>
2023-11-13 14:52:48 +0100

    update path to JAR file to access Scala from Python

--- looptrace/__init__.py
+++ looptrace/__init__.py
@@ -1,9 +1,9 @@
 """Chromatin tracing in Python, from microscopy images of FISH probes"""
 
-from pathlib import Path
+import pkg_resources
 from gertils import ExtantFile
 
 __all__ = ["LOOPTRACE_JAR_PATH"]
 
 
-LOOPTRACE_JAR_PATH = ExtantFile(Path(__file__).parent / "target"/ "scala-3.3.0" / "looptrace-assembly-0.1.0-SNAPSHOT.jar")
+LOOPTRACE_JAR_PATH = ExtantFile.from_string(pkg_resources.resource_filename(__name__, "looptrace-assembly-0.1.0-SNAPSHOT.jar"))
