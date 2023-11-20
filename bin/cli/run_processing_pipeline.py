712dba4db0670423c505de147bdefb096f15da16
Vince Reuter <vince.reuter@gmail.com>
2023-11-20 12:53:56 +0100

    move coarse drift correction to before bead generation

--- bin/cli/run_processing_pipeline.py
+++ bin/cli/run_processing_pipeline.py
@@ -49,10 +49,10 @@ class LooptracePipeline(pypiper.Pipeline):
             ("config_validation", run_config_validation, conf_only),
             ("zarr_production", run_zarr_production, conf_data_pair),
             ("psf_extraction", run_psf_extraction, conf_data_pair),
+            ("drift_correction__coarse", run_coarse_drift_correction, conf_data_pair), 
             ("bead_roi_generation", gen_all_bead_rois, conf_data_pair), # Find/define all the bead ROIs in each (FOV, frame) pair.
             # Count detected bead ROIs for each timepoint, mainly to see if anything went awry during some phase of the imaging, e.g. air bubble.
             ("bead_roi_detection_analysis", run_all_bead_roi_detection_analysis, conf_data_pair),
-            ("drift_correction__coarse", run_coarse_drift_correction, conf_data_pair), 
             ("bead_roi_partition", partition_bead_rois, conf_data_pair),
             #("nuclei_detection", run_nuclei_detection, conf_data_pair), 
             ("drift_correction__fine", run_fine_drift_correction, conf_data_pair),
