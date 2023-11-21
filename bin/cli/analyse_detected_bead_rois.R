7a2f6fa500340468402976e995d521888d0cb2c4
Vince Reuter <vince.reuter@gmail.com>
2023-11-21 13:04:03 +0100

    better output file naming, and adding heatmap for usable bead ROIs

--- bin/cli/analyse_detected_bead_rois.R
+++ bin/cli/analyse_detected_bead_rois.R
@@ -18,7 +18,14 @@ cli_parser$add_argument("--counts-files-prefix", default = "bead_rois__", help =
 cli_parser$add_argument("--counts-files-extension", default = "csv", help = "Extension for files to fine to count ROIs")
 cli_parser$add_argument("--do-not-modify-counts", action = "store_true", help = "Indicate that counts are real counts not line counts (no header, e.g.)")
 cli_parser$add_argument("--position-frame-delimiter", default = "_", help = "Delimiter between position and frame in filename")
+cli_parser$add_argument("--qc-code-column", type = "integer", default = 7, help="Column index (1-based) of the field with the QC value (empty for QC pass) in each bead ROIs file")
 opts <- cli_parser$parse_args()
+
+kBeadRoisPrefix <- "bead_roi_counts"
+
+# Infer delimiter from the extension given for each counts file.
+delimiter <- list(csv = ",", tsv = "\t")[[opts$counts_files_extension]]
+if (is.null(delimiter)) { stop("Cannot infer delimiter from file extension: ", opts$counts_files_extension) }
 
 # String maniplulation helper functions
 ## Remove the given prefix from the given target.
@@ -31,17 +38,55 @@ stripSuffix <- function(suffix, target) {
 }
 ## Parse field of view (FOV / position) and frame / hybridisation timepoint/round from given filename, based 
 ## on given prefix and extension at command-line, and delimiter assumed or given between position and frame in filename.
-parsePositionAndFrame <- function(fn, file_prefix, file_ext, pos_frame_sep) {
-    encoded <- stripPrefix(prefix = file_prefix, target = fn)
-    encoded <- stripSuffix(suffix = paste0(".", file_ext), target = encoded)
-    fields <- unlist(strsplit(encoded, pos_frame_sep))
+parsePositionAndFrame <- function(fn) {
+    encoded <- stripPrefix(prefix = opts$counts_files_prefix, target = fn)
+    encoded <- stripSuffix(suffix = paste0(".", opts$counts_files_extension), target = encoded)
+    fields <- unlist(strsplit(encoded, opts$position_frame_delimiter))
     if (length(fields) != 2) {
         stop(sprintf("Failed to parse position and frame from filename: %s. %s field(s): %s", fn, length(fields), paste0(fields, collapse = ",")))
     }
     list(position = as.integer(fields[1]), frame = as.integer(fields[2]))
 }
 
-# Build the table.
+# Count the number of QC-passing (non-failed) ROIs in a particular file (1 file per FOV + timepoint pair).
+countPassingQC <- function(f) {
+    fn <- basename(f)
+    p_and_f <- parsePositionAndFrame(fn)
+    cmd_count_filtered <- sprintf("cut -d%s -f%s %s", delimiter, opts$qc_code_column, f)
+    n_qc_pass <- sum(fread(cmd_count_filtered)[[1]] == "")
+    list(p_and_f$position, p_and_f$frame, fn, n_qc_pass)
+}
+
+# Write the count of ROIs from each (FOV, timepoint) file, either unfiltered or filtered counts.
+writeDataFile <- function(counts_table, counts_type_name) {
+    data_output_file <- file.path(opts$output_folder, sprintf("%s.%s.csv", kBeadRoisPrefix, counts_type_name))
+    message(sprintf("Writing %s bead ROI counts data: %s", counts_type_name, data_output_file))
+    write.table(counts_table, file = data_output_file, quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE)
+    data_output_file
+}
+
+# Create the heatmap for the given counts data (1 count per (FOV, time) pair).
+buildCountsHeatmap <- function(counts_table, counts_type_name) {
+    ggplot(counts_table, aes(x = frame, y = position, fill = count)) + 
+        geom_tile() + 
+        xlab("timepoint") + 
+        scale_y_continuous(breaks = round(seq(0, max(counts_table$position), by = 2), 1)) + 
+        ggtitle(sprintf("Counts, %s bead ROIs", counts_type_name)) + 
+        theme_bw()
+}
+
+# Save a bead ROIs counts plot in the current output folder (by CLI), with a particular prefix for the name to indicate content and type.
+saveCountsPlot <- function(fig, plot_type_name) {
+    plotfile <- file.path(opts$output_folder, sprintf("%s.%s.png", kBeadRoisPrefix, plot_type_name))
+    message("Saving plot: ", plotfile)
+    ggsave(filename = plotfile, plot = fig)
+    plotfile
+}
+
+# Sort by FOV and then timepoint.
+setKeyPF <- function(unkeyed) { setkey(unkeyed, position, frame) }
+
+# Build the unfiltered table.
 pattern <- sprintf("%s*.%s", opts$counts_files_prefix, opts$counts_files_extension)
 count_rois_cmd <- sprintf("wc -l %s", file.path(opts$input_folder, pattern))
 message("Building table from command: ", count_rois_cmd)
@@ -84,36 +129,27 @@ if (any(roi_counts$count < 0)) {
 message("Printing validated table")
 roi_counts
 ## Parse position and frame from each filename.
-pos_and_frame <- lapply(roi_counts$filename, function(fn) {
-    parsePositionAndFrame(
-        fn = fn, 
-        file_prefix = opts$counts_files_prefix, 
-        file_ext = opts$counts_files_extension, 
-        pos_frame_sep = opts$position_frame_delimiter
-        )
-    })
+pos_and_frame <- lapply(roi_counts$filename, parsePositionAndFrame)
+## Finalise the table by adding the position and frame information.
 roi_counts$position <- sapply(pos_and_frame, function(e) e$position)
 roi_counts$frame <- sapply(pos_and_frame, function(e) e$frame)
-## Make hybridisation timepoint / frame a factor variable rather than integer.
+roi_counts[, .(position, frame, filename, count)] # more natural/intuitive sequence of columns
+setKeyPF(roi_counts)
 
-data_output_file <- file.path(opts$output_folder, "bead_roi_counts.csv")
-message("Writing bead ROI counts data: ", data_output_file)
-write.table(roi_counts, file = data_output_file, quote = FALSE, sep = ",", row.names = FALSE, col.names = TRUE)
+# Create the filtered counts table.
+infiles <- Sys.glob(file.path(opts$input_folder, pattern))
+message(sprintf("Counting QC-passing bead ROIs in each of %s input files...", length(infiles)))
+roi_counts_filtered <- data.table(do.call(what = rbind, args = lapply(infiles, countPassingQC)))
+message("...done.")
+setKeyPF(roi_counts_filtered)
 
-saveCountsPlot <- function(fig, plotTypeName) {
-    plotfile <- file.path(opts$output_folder, sprintf("bead_roi_counts.%s.png", plotTypeName))
-    message("Saving plot: ", plotfile)
-    ggsave(filename = plotfile, plot = fig)
-    plotfile
-}
+writeDataFile(roi_counts, counts_type_name = "unfiltered")
+writeDataFile(roi_counts_filtered, counts_type_name = "filtered")
 
-message("Building (frame, FOV) bead ROI count heatmap")
-roi_counts_heatmap <- ggplot(roi_counts, aes(x = frame, y = position, fill = count)) + 
-    geom_tile() + 
-    xlab("timepoint") + 
-    scale_y_continuous(breaks = round(seq(0, max(roi_counts$position), by = 2), 1)) + 
-    ggtitle("Counts, all detected bead ROIs") + 
-    theme_bw()
-saveCountsPlot(fig = roi_counts_heatmap, plotTypeName = "detected.heatmap")
+message("Building (frame, FOV) bead ROI count heatmaps")
+roi_counts_heatmap <- buildCountsHeatmap(roi_counts, "unfiltered")
+saveCountsPlot(fig = roi_counts_heatmap, plot_type_name = "unfiltered.heatmap")
+roi_counts_heatmap <- buildCountsHeatmap(roi_counts_filtered, "filtered")
+saveCountsPlot(fig = roi_counts_heatmap, plot_type_name = "filtered.heatmap")
 
 message("Done!")
