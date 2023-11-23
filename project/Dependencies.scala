8918a7c66bda9fab4eaedc844419553ceb10676b
Vince Reuter <vince.reuter@gmail.com>
2023-11-24 00:29:51 +0100

    try adding Java reader of zarr

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,10 @@ object Dependencies {
     
     /* Core libraries */
     lazy val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
+    lazy val cdmZarr = "edu.ucar" % "cdm-zarr" % "5.5.3"
     lazy val mouse = "org.typelevel" %% "mouse" % "1.2.1"
     lazy val os = "com.lihaoyi" %% "os-lib" % "0.9.2"
+    lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.10"
     lazy val scopt = "com.github.scopt" %% "scopt" % "4.1.0"
     lazy val uJson = "com.lihaoyi" %% "ujson" % "3.1.3"
     lazy val uPickle = "com.lihaoyi" %% "upickle" % "3.1.3"
@@ -16,7 +18,6 @@ object Dependencies {
     /* Test dependencies */
     lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
     lazy val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
-    lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.10"
     lazy val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
     lazy val scalatestScalacheck = "org.scalatestplus" %% "scalacheck-1-17" % "3.2.17.0"
 
