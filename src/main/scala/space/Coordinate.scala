9a230e9e089af8eb9cdfc4e577327863a9771906
Vince Reuter <vince.reuter@gmail.com>
2023-11-24 17:36:45 +0100

    Order[Coordinate] instances, type inequality with =:= and NotGiven, type lambdas, and test for Coordinate subtypes

--- src/main/scala/space/Coordinate.scala
+++ src/main/scala/space/Coordinate.scala
@@ -1,9 +1,19 @@
 package at.ac.oeaw.imba.gerlich.looptrace.space
 
+import scala.util.NotGiven
+import cats.Order
+
 sealed trait Coordinate { def get: Double }
+object Coordinate:
+    given orderForCoordinate[C <: Coordinate : [C] =>> NotGiven[C =:= Coordinate]]: Order[C] = Order.by(_.get)
 
 final case class XCoordinate(get: Double) extends Coordinate
+object XCoordinate:
+    given orderForX: Order[XCoordinate] = Order.by(_.get)
 
 final case class YCoordinate(get: Double) extends Coordinate
-
+object YCoordinate:
+    given orderForY: Order[YCoordinate] = Order.by(_.get)
 final case class ZCoordinate(get: Double) extends Coordinate
+object ZCoordinate:
+    given orderForZ: Order[ZCoordinate] = Order.by(_.get)
