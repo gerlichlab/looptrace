181b15aabff9a3fa3ad6d5389c24a410fb304ae9
Vince Reuter <vince.reuter@gmail.com>
2023-11-29 13:26:57 +0100

    modularisation of function

--- src/main/scala/CsvHelpers.scala
+++ src/main/scala/CsvHelpers.scala
@@ -31,11 +31,7 @@ object CsvHelpers:
     
     /** Safely write rows to CSV with header, ensuring each has exactly the header's fields, and is ordered accordingly. */
     def writeAllCsv(f: os.Path, header: List[String], rows: Iterable[CsvRow], handleExtant: ExtantOutputHandler): Boolean = {
-        val maybeWrite: Either[Throwable, os.Source => Boolean] = handleExtant match {
-            case ExtantOutputHandler.Skip if os.isFile(f) => ((_: os.Source) => false).asRight
-            case ExtantOutputHandler.Fail if os.isFile(f) => FileAlreadyExistsException(f.toString).asLeft
-            case _ => (os.write.over(f, (_: os.Source))).returning(true).asRight
-        }
+        val maybeWrite: Either[Throwable, os.Source => Boolean] = handleExtant.getSimpleWriter(f)
         val maybeLines: Either[Throwable, os.Source] = prepCsvWrite(header)(rows).map(recs => (header :: recs).map(_.mkString(",") ++ "\n"))
         (maybeWrite <*> maybeLines).fold(throw _, identity)
     }
--- src/main/scala/ExtantOutputHandler.scala
+++ src/main/scala/ExtantOutputHandler.scala
@@ -2,11 +2,19 @@ package at.ac.oeaw.imba.gerlich.looptrace
 
 import java.io.File
 import java.nio.file.FileAlreadyExistsException
+import cats.syntax.all.*
 import scopt.Read
 
 /** How a program should handle trying to write output when the target already exists. */
 enum ExtantOutputHandler:
     case Overwrite, Skip, Fail
+
+    /** Get a total function taking an {@code os.Source} and producing a result saying if the target will be written. */
+    def getSimpleWriter(target: os.Path): Either[FileAlreadyExistsException, os.Source => Boolean] = this match {
+        case ExtantOutputHandler.Skip if os.isFile(target) => ((_: os.Source) => false).asRight
+        case ExtantOutputHandler.Fail if os.isFile(target) => FileAlreadyExistsException(target.toString).asLeft
+        case _ => (os.write.over(target, (_: os.Source))).returning(true).asRight
+    }
 
     /** Print a message about why processing can proceed, or provide a {@code Left}-wrapped message or exception */
     def prepareToWrite(f: os.Path): Either[Unit | FileAlreadyExistsException, os.Path] = {
