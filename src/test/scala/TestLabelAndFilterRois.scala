b408bc7f3aa4755b958d7471df6e962e323ef6a1
Vince Reuter <vince.reuter@gmail.com>
2023-12-01 10:43:19 +0100

    stub 2 tests for ROI labeling and filtering

---
+++ src/test/scala/TestLabelAndFilterRois.scala
@@ -0,0 +1,12 @@
+package at.ac.oeaw.imba.gerlich.looptrace
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.*
+import org.scalatest.funsuite.AnyFunSuite
+
+
+/** Tests for the filtration of the individual supports (single FISH probes) of chromatin fiber traces */
+class TestLabelAndFilterRois extends AnyFunSuite, GenericSuite, ScalacheckSuite, should.Matchers:
+    test("Spot distance comparison uses drift correction when available.") { pending }
+    test("Spot distance comparison requires drift correction") { pending }
+end TestLabelAndFilterRois
