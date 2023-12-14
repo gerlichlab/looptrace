3b0d1b0f04eb1831990d262b267660dd1cecc8d5
Vince Reuter <vince.reuter@gmail.com>
2023-12-14 11:47:21 +0100

    bump mouse version and parameterise Scalacheck versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,11 @@ import sbt._
 object Dependencies {
     /* Versions */
     lazy val scalatestVersion = "3.2.17"
+    lazy val (scalacheckMajor, scalacheckMinor) = ("1", "17")
     
     /* Core libraries */
     lazy val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
-    lazy val mouse = "org.typelevel" %% "mouse" % "1.2.1"
+    lazy val mouse = "org.typelevel" %% "mouse" % "1.2.2"
     lazy val os = "com.lihaoyi" %% "os-lib" % "0.9.2"
     lazy val scalaCsv = "com.github.tototoshi" %% "scala-csv" % "1.3.10"
     lazy val scopt = "com.github.scopt" %% "scopt" % "4.1.0"
@@ -15,9 +16,9 @@ object Dependencies {
     lazy val uPickle = "com.lihaoyi" %% "upickle" % "3.1.3"
 
     /* Test dependencies */
-    lazy val scalacheck = "org.scalacheck" %% "scalacheck" % "1.17.0"
+    lazy val scalacheck = "org.scalacheck" %% "scalacheck" % s"$scalacheckMajor.$scalacheckMinor.0"
     lazy val scalactic = "org.scalactic" %% "scalactic" % scalatestVersion
     lazy val scalatest = "org.scalatest" %% "scalatest" % scalatestVersion
-    lazy val scalatestScalacheck = "org.scalatestplus" %% "scalacheck-1-17" % "3.2.17.0"
+    lazy val scalatestScalacheck = "org.scalatestplus" %% s"scalacheck-$scalacheckMajor-$scalacheckMinor" % "3.2.17.0"
 
 }
