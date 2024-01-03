dacaa0e9c5bfeb8879e4850754183a46879d5c41
Vince Reuter <vince.reuter@gmail.com>
2024-01-03 15:13:01 +0100

    remove unused helpers

--- src/main/scala/space/Coordinate.scala
+++ src/main/scala/space/Coordinate.scala
@@ -15,21 +15,8 @@ end Coordinate
 /** Notion of point along x-axis / x-dimension in a space */
 final case class XCoordinate(get: Double) extends Coordinate
 
-/** Helpers for working with coordinates along the x-axis / x-dimension */
-object XCoordinate:
-    given orderForX: Order[XCoordinate] = Order.by(_.get)
-end XCoordinate
-
 /** Notion of point along y-axis / y-dimension in a space */
 final case class YCoordinate(get: Double) extends Coordinate
 
-/** Helpers for working with coordinates along the y-axis / y-dimension */
-object YCoordinate:
-    given orderForY: Order[YCoordinate] = Order.by(_.get)
-
 /** Notion of point along z-axis / z-dimension in a space */
 final case class ZCoordinate(get: Double) extends Coordinate
-
-/** Helpers for working with coordinates along the z-axis / z-dimension */
-object ZCoordinate:
-    given orderForZ: Order[ZCoordinate] = Order.by(_.get)
