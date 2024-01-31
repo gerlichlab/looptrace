ecb054a1b7e1fd913eafa68ac7e31a6647f98be8
Vince Reuter <vince.reuter@gmail.com>
2024-01-31 12:20:49 +0100

    compute the numpy arrays from dask

--- bin/cli/assign_spots_to_nucs.py
+++ bin/cli/assign_spots_to_nucs.py
@@ -55,10 +55,10 @@ def workflow(
         filter_kwargs = {"nuc_drifts": nuc_drifts, "nuc_target_frame": H.config['nuc_ref_frame'], "spot_drifts": spot_drifts}
         if NucDetector.MASKS_KEY in H.images:
             logger.info(f"Assigning nuclei labels for sports from position: {pos}")
-            rois = ip.filter_rois_in_nucs(rois, nuc_label_img=H.images[NucDetector.MASKS_KEY][i][0, 0], new_col='nuc_label', **filter_kwargs)
+            rois = ip.filter_rois_in_nucs(rois, nuc_label_img=H.images[NucDetector.MASKS_KEY][i][0, 0].compute(), new_col='nuc_label', **filter_kwargs)
         if NucDetector.CLASSES_KEY in H.images:
             logger.info(f"Assigning nuclei classes for spots from position: {pos}")
-            rois = ip.filter_rois_in_nucs(rois, nuc_label_img=H.images[NucDetector.CLASSES_KEY][i][0, 0], new_col='nuc_class', **filter_kwargs)
+            rois = ip.filter_rois_in_nucs(rois, nuc_label_img=H.images[NucDetector.CLASSES_KEY][i][0, 0].compute(), new_col='nuc_class', **filter_kwargs)
         all_rois.append(rois.copy())
     
     outfile = H.nuclei_labeled_spots_file_path
