dfd4a8523230d7e7d46b404f6b4075a76c0ad7f6
Vince Reuter <vince.reuter@gmail.com>
2024-02-04 18:00:55 +0100

    more enforcement of relation between nuclei detection method, z-slice for nuclei, and 3D or not; #241, #250, #247

--- tests/test_nucleus_detection.py
+++ tests/test_nucleus_detection.py
@@ -1,17 +1,24 @@
 """Tests for detection and segmentation of nuclei"""
 
 import copy
+import itertools
 from operator import itemgetter
+import os
 from pathlib import Path
+import shutil
 from typing import *
 
+import dask.array as da
+import hypothesis as hyp
 import numpy as np
+from numpy.random import uniform as runif
 import pytest
 import yaml
 
 from looptrace import ArrayDimensionalityError, MissingImagesError
 from looptrace.ImageHandler import ImageHandler
-from looptrace.NucDetector import NucDetector
+from looptrace.NucDetector import NucDetector, SegmentationMethod
+from looptrace.image_io import write_jvm_compatible_zarr_store
 
 __author__ = "Vince Reuter"
 
@@ -34,16 +41,11 @@ def complete_config_data(tmp_path, base_conf) -> dict:
     return conf_data
 
 
-def get_image_handler(conf_data: dict, conf_file: Path, images_folder: Optional[Path] = None) -> ImageHandler:
-    """Write config data to file, then parse to image handler."""
-    with open(conf_file, "w") as fh:
-        yaml.dump(conf_data, fh)
-    return ImageHandler(conf_file, images_folder)
-
-
 def get_nuc_detector(conf_data: dict, conf_file: Path, images_folder: Optional[Path] = None) -> NucDetector:
     """Write config data to file, then parse to nuclei detector, through image handler."""
-    return NucDetector(get_image_handler(conf_data=conf_data, conf_file=conf_file, images_folder=images_folder))
+    with open(conf_file, "w") as fh:
+        yaml.dump(conf_data, fh)
+    return NucDetector(ImageHandler(conf_file, images_folder))
 
 
 @pytest.mark.parametrize(
@@ -100,21 +102,26 @@ def test_position_names_images_list_relationship(complete_config_data, tmp_path)
 
 @pytest.mark.parametrize(
     ["img_by_name", "expect"], [
+        # First, the good case, with 4D image arrays (channel, z, y, x)
         (
             {"P0004.zarr": np.zeros(shape=(2, 2, 2, 2)), "P0003.zarr": np.ones(shape=(3, 3, 3, 3))}, 
             [np.ones(shape=(3, 3, 3, 3)), np.zeros(shape=(2, 2, 2, 2))]
         ), 
+        # Then, the bad case where there's no channel dimension.
         (
             {"P0002.zarr": np.zeros(shape=(2, 2, 2)), "P0001.zarr": np.ones(shape=(2, 2, 2))}, 
             ArrayDimensionalityError("2 images with bad shape (length not equal to 4)")
         ), 
+        # Finally, the bad case where there's time dimension in addition to the 4 required ones.
         (
             {"P0002.zarr": np.zeros(shape=(2, 2, 2, 2, 2)), "P0001.zarr": np.ones(shape=(2, 2, 2, 2, 2))}, 
             ArrayDimensionalityError("2 images with bad shape (length not equal to 4)")
         ), 
     ]
     )
-def test_bad_image_dimensionality__generates_expected_error(complete_config_data, img_by_name, expect, tmp_path):
+def test_bad_image_dimensionality__generates_expected_error__enforcing_single_timepoint_issue_250(
+    complete_config_data, img_by_name, expect, tmp_path,
+    ):
     input_key = complete_config_data["nuc_input_name"]
     N = get_nuc_detector(complete_config_data, tmp_path / "conf.yaml")
     img_names, img_arrays = zip(*sorted(img_by_name.items(), key=itemgetter(0)))
@@ -131,9 +138,70 @@ def test_bad_image_dimensionality__generates_expected_error(complete_config_data
         assert all((obs == exp).all() for obs, exp in zip(N.input_images, expect))
 
 
-@pytest.mark.skip("not implemented")
-def test_nuc_detector__generates_image_of_proper_dimension():
-    pass
+@pytest.mark.parametrize("config_update", [
+    {NucDetector.DETECTION_METHOD_KEY: method, NucDetector.KEY_3D: do_3d, "nuc_slice": z_slice} 
+    for method, do_3d, z_slice 
+    in itertools.product(["cellpose", "nuclei", "threshold"], [False, True], [-1, 0, 1])
+])
+@hyp.given(num_pos=hyp.strategies.integers(min_value=1, max_value=3))
+@hyp.settings(suppress_health_check=(hyp.HealthCheck.function_scoped_fixture, ), deadline=None)
+# NB: for this test we suppress the health check about function-scoped fixture because we clear the 
+#     images folder at the end, which should make new data examples independent.
+#     We also set deadline=None since the first run will likely take longer, as folders are made.
+def test_nuc_detector__generates_image_of_proper_dimension(complete_config_data, config_update, num_pos, tmp_path):
+    """This tests the generation of images for nuclei segmentation. Cellpose expects single-channel."""
+    # Prepare the config data and the nucleus detector instance.
+    conf_data = copy.deepcopy(complete_config_data)
+    conf_data.update(config_update)
+    input_key = conf_data["nuc_input_name"]
+    N = get_nuc_detector(conf_data=conf_data, conf_file=tmp_path / "conf.yaml")
+    
+    # pretest to ensure we have expected 3D setting
+    assert N.do_in_3d == config_update[NucDetector.KEY_3D]
+    
+    # With no images, we should get the expected error.
+    assert not N.has_images_for_segmentation
+    with pytest.raises(MissingImagesError) as err_ctx:
+        N.images_for_segmentation
+    assert str(err_ctx.value) == f"No images available at all; was {NucDetector.__name__} created without an images folder?"
+    
+    # Generate the inputs.
+    N.image_handler.images = {}
+    N.image_handler.image_lists = {}
+    gen_img = lambda: runif(size=(2, 3, 4, 6))
+    inputs = [(f"P000{i}", gen_img()) for i in range(num_pos)]
+    images_folder = tmp_path / "images"
+    write_jvm_compatible_zarr_store(inputs, root_path=images_folder, dtype=np.uint16)
+    input_names, input_images = zip(*inputs)
+    N.image_handler.images[input_key] = input_images
+    N.image_handler.image_lists[input_key] = input_names
+    
+    # Still with just raw--no preprocessed--images, we should get the expected error.
+    assert not N.has_images_for_segmentation
+    with pytest.raises(MissingImagesError) as err_ctx:
+        N.images_for_segmentation
+    assert str(err_ctx.value) == f"No images available ({NucDetector.SEGMENTATION_IMAGES_KEY}) as preprocessed input for nuclei segmentation!"
+    
+    # Prepare for and do the image generation.
+    N.image_handler.image_save_path = images_folder
+    N.generate_images_for_segmentation()
+    
+    # Now the preprocessed images should be present and as expected...only AFTER image reloading.
+    N.image_handler.image_path = images_folder
+    assert not N.has_images_for_segmentation
+    N.image_handler.read_images()
+    assert N.has_images_for_segmentation
+    get_single_channel = lambda img: img[conf_data["nuc_channel"]]
+    all_imgs_obs = N.images_for_segmentation
+    if conf_data[NucDetector.KEY_3D] or N.segmentation_method == SegmentationMethod.THRESHOLD:
+        get_exp = get_single_channel
+    else:
+        get_single_z = lambda img: da.max(img, axis=0) if conf_data["nuc_slice"] == -1 else img[conf_data["nuc_slice"]]
+        get_exp = lambda img: get_single_z(get_single_channel(img))
+    all_imgs_exp = map(get_exp, input_images)
+    assert [img.shape for img in all_imgs_obs] == [img.shape for img in all_imgs_exp]
+    assert all((obs == exp).all() for obs, exp in zip(all_imgs_obs, all_imgs_exp))
+    shutil.rmtree(images_folder)
 
 
 @pytest.mark.skip("not implemented")
