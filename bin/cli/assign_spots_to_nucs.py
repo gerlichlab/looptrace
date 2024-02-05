00d0fc46ac4f9d1ec5cb7e8039ad7000d8e5cddd
Vince Reuter <vince.reuter@gmail.com>
2024-02-05 03:03:18 +0100

    fix writing of nuclei-filtered spots file

--- bin/cli/assign_spots_to_nucs.py
+++ bin/cli/assign_spots_to_nucs.py
@@ -154,8 +154,8 @@ def workflow(
         all_rois = pd.concat(all_rois).sort_values(["position", "frame"])
         logger.info(f"Writing nuclei-labeled spots file: {outfile}")
         all_rois.to_csv(outfile)
-        logger.info(f"Writing nuclei-filtered spots file: {H.nuclei_labeled_spots_file_path}")
-        all_rois.to_csv(all_rois[all_rois[NUC_LABEL_COL] != 0].drop(NUC_LABEL_COL, axis=1))
+        logger.info(f"Writing nuclei-filtered spots file: {H.nuclei_filtered_spots_file_path}")
+        all_rois[all_rois[NUC_LABEL_COL] != 0].drop(NUC_LABEL_COL, axis=1).to_csv(H.nuclei_filtered_spots_file_path)
 
     logger.info("Done!")
 
