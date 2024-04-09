69ad4a660546aae1c7907021a27d85d2ca71283c
Vince Reuter <vince.reuter@gmail.com>
2024-04-09 11:08:37 +0200

    add test to check that pipeline steps are up-to-date

---
+++ test_docs/test_pipeline_execution_control_and_resart_doc.py
@@ -0,0 +1,35 @@
+"""Test that the document for pipeline execution control is up-to-date."""
+
+from pathlib import Path
+import subprocess
+
+__author__ = "Vince Reuter"
+__credits__ = ["Vince Reuter"]
+
+
+PROJECT_ROOT = Path(__file__).parent.parent
+
+
+def test_pipline_control_flow_doc_is_current(tmp_path):
+    # Pretests
+    target = tmp_path / "doc.md"
+    assert not target.exists()
+    current = PROJECT_ROOT / "docs" / "pipeline-execution-control-and-restart.md"
+    assert current.is_file()
+    
+    # Create snapshot of target doc.
+    builder_program = PROJECT_ROOT / "bin" / "cli" / "generate_excution_control_document.py"
+    cmd = ["python", str(builder_program), "-O", str(target)]
+    print(f"Running command: {' '.join(cmd)}")
+    subprocess.check_call(cmd)
+    
+    # Check that the current doc matches that snapshot (current code state).
+    assert target.is_file()
+    with open(target, "r") as fh:
+        snapshot_lines = fh.readlines()
+    with open(current, "r") as fh:
+        current_lines = fh.readlines()
+    keep_line = lambda line: not line.startswith("<!--")
+    snapshot_lines = list(filter(keep_line, snapshot_lines))
+    current_lines = list(filter(keep_line, current_lines))
+    assert snapshot_lines == current_lines
