e35fec12fc8a9383f1f6bac780287196b7f520bd
Vince Reuter <vince.reuter@gmail.com>
2024-05-07 19:52:11 +0200

    first-pass at #237, with stubbed tests

---
+++ tests/integration/test_extract_spots_table.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.skip("not implemented")
+def test_only_region_timepoints_in_locus_grouping_have_records_in_spot_extraction_table():
+    pass
+
+
+@pytest.mark.skip("not implemented")
+def test_only_locus_timepoints_in_locus_grouping_have_records_in_spot_extraction_table():
+    pass
