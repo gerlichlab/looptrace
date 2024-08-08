e39d1f00c2de8c551c2e6ba88de63dbb007f8b9b
Vince Reuter <vince.reuter@gmail.com>
2024-08-08 14:26:55 +0200

    implement both new and old decoders for ROI, as transition for spotfishing; bit of Scaladoc

--- src/main/scala/csv/instances/RoiCsvInstances.scala
+++ src/main/scala/csv/instances/RoiCsvInstances.scala
@@ -2,13 +2,16 @@ package at.ac.oeaw.imba.gerlich.looptrace.csv
 package instances
 
 import fs2.data.csv.CsvRowDecoder
+
 import at.ac.oeaw.imba.gerlich.gerlib.io.csv.getCsvRowDecoderForProduct2
 import at.ac.oeaw.imba.gerlich.gerlib.io.csv.instances.all.given
+
 import at.ac.oeaw.imba.gerlich.looptrace.DetectedSpotRoi
 import at.ac.oeaw.imba.gerlich.looptrace.csv.instances.spatial.given
+import at.ac.oeaw.imba.gerlich.looptrace.space.BoundingBox
 
 /** Typeclass instances related to CSV, for ROI-related data types */
 trait RoiCsvInstances:
-    given CsvRowDecoder[DetectedSpotRoi, String] = 
+    given csvRowDecoderForDetectedSpotRoi(using CsvRowDecoder[BoundingBox, String]): CsvRowDecoder[DetectedSpotRoi, String] = 
         getCsvRowDecoderForProduct2(DetectedSpotRoi.apply)
 end RoiCsvInstances
