10f95302df478a8db8f9970ef1f4345a9d3888c6
Vince Reuter <vince.reuter@gmail.com>
2024-08-09 16:37:25 +0200

    add CSV decoder for a couple of ROI types

--- src/main/scala/csv/instances/RoiCsvInstances.scala
+++ src/main/scala/csv/instances/RoiCsvInstances.scala
@@ -3,14 +3,26 @@ package instances
 
 import fs2.data.csv.CsvRowDecoder
 
+import at.ac.oeaw.imba.gerlich.gerlib.cell.NuclearDesignation
 import at.ac.oeaw.imba.gerlich.gerlib.io.csv.getCsvRowDecoderForProduct2
 import at.ac.oeaw.imba.gerlich.gerlib.io.csv.instances.all.given
 
 import at.ac.oeaw.imba.gerlich.looptrace.DetectedSpotRoi
+import at.ac.oeaw.imba.gerlich.looptrace.NucleusLabelAttemptedRoi
 import at.ac.oeaw.imba.gerlich.looptrace.space.BoundingBox
 
 /** Typeclass instances related to CSV, for ROI-related data types */
 trait RoiCsvInstances:
-    given csvRowDecoderForDetectedSpotRoi(using CsvRowDecoder[BoundingBox, String]): CsvRowDecoder[DetectedSpotRoi, String] = 
+    private type Header = String
+    
+    given csvRowDecoderForDetectedSpotRoi(
+        using CsvRowDecoder[BoundingBox, Header]
+    ): CsvRowDecoder[DetectedSpotRoi, Header] = 
         getCsvRowDecoderForProduct2(DetectedSpotRoi.apply)
+
+    given csvRowDecoderForNucleusLabelAttemptedRoi(using 
+        CsvRowDecoder[DetectedSpotRoi, Header],
+        CsvRowDecoder[NuclearDesignation, Header]
+    ): CsvRowDecoder[NucleusLabelAttemptedRoi, Header] = 
+        getCsvRowDecoderForProduct2(NucleusLabelAttemptedRoi.apply)
 end RoiCsvInstances
