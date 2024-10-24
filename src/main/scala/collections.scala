c5227122eb9ec5b9cf8ed9fc6d5ce8a8a76d4dd4
Vince Reuter <vince.reuter@gmail.com>
2024-10-24 14:58:30 +0200

    move generic tool to gerlib, and depend on the development version of that project which has this migrated code

--- src/main/scala/collections.scala
+++ src/main/scala/collections.scala
@@ -20,24 +20,4 @@ package object collections:
 
     extension [X](xs: NonEmptySet[X])
         def ++(ys: Set[X])(using Ordering[X]): NonEmptySet[X] = ys.toNonEmptySet.fold(xs)(xs | _)
-
-    type AtLeast2[C[*], E] = C[E] :| MinLength[2]
-
-    object AtLeast2:
-        def apply[X](x: X, xs: NonEmptyList[X]): AtLeast2[List, X] = 
-            (x :: xs).toList.refineUnsafe[MinLength[2]]
-        
-        inline def either[C[*], X](xs: C[X])(using inline: Constraint[C[X], MinLength[2]]): Either[String, AtLeast2[C, X]] = 
-            xs.refineEither[MinLength[2]]
-        
-        extension [X](xs: AtLeast2[Set, X])
-            infix def +(x: X): AtLeast2[Set, X] = 
-                (xs + x).refineUnsafe[MinLength[2]]
-
-        private type AtLeast2FixedC[C[*]] = [X] =>> AtLeast2[C, X]
-        
-        given functorForAtLeast2[C[*]: Functor]: Functor[AtLeast2FixedC[C]] with
-            override def map[A, B](fa: AtLeast2FixedC[C][A])(f: A => B): AtLeast2FixedC[C][B] = 
-                fa.map(f)
-    end AtLeast2
 end collections
