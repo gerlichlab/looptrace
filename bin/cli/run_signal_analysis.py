12b41aad6d4687c1c3884789158deea090962915
Vince Reuter <vince.reuter@gmail.com>
2024-11-24 17:18:19 +0100

    traverse over parsing of signal analysis specs

--- bin/cli/run_signal_analysis.py
+++ bin/cli/run_signal_analysis.py
@@ -9,16 +9,14 @@ import logging
 from operator import itemgetter
 from pathlib import Path
 import sys
-from typing import Iterable, Mapping, TypeAlias, TypeVar
+from typing import Callable, Iterable, Mapping, TypeAlias, TypeVar
 
-from expression import Option, Result, compose, snd
+from expression import Option, Result, compose, curry_flip, snd
 from expression.collections import Seq, seq
 from expression.core import option, result
 from gertils import ExtantFile, compute_pixel_statistics
 from gertils.geometry import ImagePoint3D
-from gertils.pixel_value_statistics import Numeric as PixelStatValue
 from gertils.types import ImagingChannel
-import numpy.typing as npt
 import pandas as pd
 from spotfishing.roi_tools import get_centroid_from_record
 
@@ -28,7 +26,8 @@ from looptrace.ImageHandler import ImageHandler
 from looptrace.SpotPicker import SpotPicker
 from looptrace.utilities import find_first_option, get_either, wrap_exception, wrap_error_message
 
-_A = TypeVar("_")
+_A = TypeVar("_A")
+_B = TypeVar("_B")
 
 SIGNAL_CHANNEL_COLUMN = "signalChannel"
 
@@ -131,70 +130,86 @@ def workflow(*, rounds_config: ExtantFile, params_config: ExtantFile, maybe_sign
             logging.info("Parsing signal analysis configuration: %s", conf_path)
             with conf_path.open(mode="r") as fh:
                 conf_data = json.load(fh)
-                if not isinstance(conf_data, list):
-                    raise TypeError(f"Parsed signal config data (from {conf_path}) is {type(conf_data).__name__}")
-                try:
-                    analysis_specs: list[AnalyticalSpecification] = list(map(AnalyticalSpecification.from_mapping, conf_data))
-                except (TypeError, ValueError) as e:
-                    logging.error("Failed to parse analytical specifications (from %s): %s", conf_path, e)
-                    raise
+            if not isinstance(conf_data, list):
+                raise TypeError(f"Parsed signal config data (from {conf_path}) is {type(conf_data).__name__}")
+            match traverse_through_either(AnalyticalSpecification.from_mapping)(conf_data):
+                case result.Result(tag="error", error=messages):
+                    raise Exception(f"Failed to parse analytical specifications (from {conf_path}): {messages}")
+                case result.Result(tag="ok", ok=analysis_specs):
+                    H = ImageHandler(rounds_config=rounds_config, params_config=params_config)
+                    S = SpotPicker(H)
 
-            H = ImageHandler(rounds_config=rounds_config, params_config=params_config)
-            S = SpotPicker(H)
+                    spot_drift_file: Path = H.drift_correction_file__coarse
+                    logging.info("Reading nuclei drift file: %s", spot_drift_file)
+                    all_spot_drifts: pd.DataFrame = pd.read_csv(spot_drift_file, index_col=False)
 
-            spot_drift_file: Path = H.drift_correction_file__coarse
-            logging.info("Reading nuclei drift file: %s", spot_drift_file)
-            all_spot_drifts: pd.DataFrame = pd.read_csv(spot_drift_file, index_col=False)
+                    nuclei_drift_file: Path = H.nuclei_coarse_drift_correction_file
+                    logging.info("Reading nuclei drift file: %s", nuclei_drift_file)
+                    all_nuclei_drifts: pd.DataFrame = pd.read_csv(nuclei_drift_file, index_col=False)
 
-            nuclei_drift_file: Path = H.nuclei_coarse_drift_correction_file
-            logging.info("Reading nuclei drift file: %s", nuclei_drift_file)
-            all_nuclei_drifts: pd.DataFrame = pd.read_csv(nuclei_drift_file, index_col=False)
+                    # TODO: discard regional spot timepoints from the bigger collection: https://github.com/gerlichlab/looptrace/issues/376
+                    for spec in analysis_specs:
+                        # Get the ROIs of this type.
+                        roi_type: RoiType = spec.roi_type
+                        logging.info("Analyzing signal for ROI type '%s'", roi_type.name)
+                        rois_file: Path = getattr(H, roi_type.file_attribute_on_image_handler)
+                        all_rois: pd.DataFrame = pd.read_csv(rois_file, index_col=False)
+                        
+                        # Build up the records for this ROI type, for all FOVs.
+                        by_raw_channel: Mapping[int, list[dict]] = defaultdict
+                        # TODO: refactor with https://github.com/gerlichlab/gertils/issues/32
+                        for fov, img in S.iter_fov_img_pairs():
+                            logging.info(f"Analysing signal for FOV: {fov}")
+                            nuc_drift_curr_fov: pd.DataFrame = all_nuclei_drifts[all_nuclei_drifts[FIELD_OF_VIEW_COLUMN] == fov]
+                            logging.debug(f"Shape of nuclei drifts ({type(nuc_drift_curr_fov).__name__}): {nuc_drift_curr_fov.shape}")
+                            spot_drifts_curr_fov: pd.DataFrame = all_spot_drifts[all_spot_drifts[FIELD_OF_VIEW_COLUMN] == fov]
+                            logging.debug(f"Shape of spot drifts ({type(spot_drifts_curr_fov).__name__}): {spot_drifts_curr_fov.shape}")
+                            rois: pd.DataFrame = all_rois[all_rois[FIELD_OF_VIEW_COLUMN] == fov]
+                            logging.debug("ROI count: %d", rois.shape[0])
+                            for _, r in rois.iterrows():
+                                spot_drift = spot_drifts_curr_fov[spot_drifts_curr_fov[TIMEPOINT_COLUMN] == r[TIMEPOINT_COLUMN]]
+                                pt0: ImagePoint3D = get_centroid_from_record(r)
+                                dc_pt: ImagePoint3D = ImagePoint3D(
+                                    z=pt0.z - nuc_drift_curr_fov[Z_PX_COARSE] + spot_drift[Z_PX_COARSE], 
+                                    y=pt0.y - nuc_drift_curr_fov[Y_PX_COARSE] + spot_drift[Y_PX_COARSE], 
+                                    x=pt0.x - nuc_drift_curr_fov[X_PX_COARSE] + spot_drift[X_PX_COARSE], 
+                                )
+                                for stats in compute_pixel_statistics(
+                                    img=img,
+                                    pt=dc_pt,
+                                    channels=spec.channels, 
+                                    diameter=spec.roi_diameter,
+                                    channel_column=SIGNAL_CHANNEL_COLUMN,
+                                ):
+                                    ch: int = stats[SIGNAL_CHANNEL_COLUMN]
+                                    by_raw_channel[ch].append({**r.to_dict(), **stats})
+                        
+                        # Write the output file for this ROI type, across all FOVs.
+                        for raw_channel, records in sorted(by_raw_channel.items(), key=itemgetter(0)):
+                            stats_frame: pd.DataFrame = pd.DataFrame(records)
+                            fn = f"signal_analysis__rois_{roi_type.name}__channel_{raw_channel}.csv"
+                            outfile: Path = Path(H.analysis_path) / fn
+                            logging.info("Writing output file: %s", outfile)
+                            stats_frame.to_csv(outfile, index=False)
 
-            # TODO: discard regional spot timepoints from the bigger collection: https://github.com/gerlichlab/looptrace/issues/376
-            for spec in analysis_specs:
-                # Get the ROIs of this type.
-                roi_type: RoiType = spec.roi_type
-                logging.info("Analyzing signal for ROI type '%s'", roi_type.name)
-                rois_file: Path = getattr(H, roi_type.file_attribute_on_image_handler)
-                all_rois: pd.DataFrame = pd.read_csv(rois_file, index_col=False)
-                
-                # Build up the records for this ROI type, for all FOVs.
-                by_raw_channel: Mapping[int, list[dict]] = defaultdict
-                # TODO: refactor with https://github.com/gerlichlab/gertils/issues/32
-                for fov, img in S.iter_fov_img_pairs():
-                    logging.info(f"Analysing signal for FOV: {fov}")
-                    nuc_drift_curr_fov: pd.DataFrame = all_nuclei_drifts[all_nuclei_drifts[FIELD_OF_VIEW_COLUMN] == fov]
-                    logging.debug(f"Shape of nuclei drifts ({type(nuc_drift_curr_fov).__name__}): {nuc_drift_curr_fov.shape}")
-                    spot_drifts_curr_fov: pd.DataFrame = all_spot_drifts[all_spot_drifts[FIELD_OF_VIEW_COLUMN] == fov]
-                    logging.debug(f"Shape of spot drifts ({type(spot_drifts_curr_fov).__name__}): {spot_drifts_curr_fov.shape}")
-                    rois: pd.DataFrame = all_rois[all_rois[FIELD_OF_VIEW_COLUMN] == fov]
-                    logging.debug("ROI count: %d", rois.shape[0])
-                    for _, r in rois.iterrows():
-                        spot_drift = spot_drifts_curr_fov[spot_drifts_curr_fov[TIMEPOINT_COLUMN] == r[TIMEPOINT_COLUMN]]
-                        pt0: ImagePoint3D = get_centroid_from_record(r)
-                        dc_pt: ImagePoint3D = ImagePoint3D(
-                            z=pt0.z - nuc_drift_curr_fov[Z_PX_COARSE] + spot_drift[Z_PX_COARSE], 
-                            y=pt0.y - nuc_drift_curr_fov[Y_PX_COARSE] + spot_drift[Y_PX_COARSE], 
-                            x=pt0.x - nuc_drift_curr_fov[X_PX_COARSE] + spot_drift[X_PX_COARSE], 
-                        )
-                        for stats in compute_pixel_statistics(
-                            img=img,
-                            pt=dc_pt,
-                            channels=spec.channels, 
-                            diameter=spec.roi_diameter,
-                            channel_column=SIGNAL_CHANNEL_COLUMN,
-                        ):
-                            ch: int = stats[SIGNAL_CHANNEL_COLUMN]
-                            by_raw_channel[ch].append({**r.to_dict(), **stats})
-                
-                # Write the output file for this ROI type, across all FOVs.
-                for raw_channel, records in sorted(by_raw_channel.items(), key=itemgetter(0)):
-                    stats_frame: pd.DataFrame = pd.DataFrame(records)
-                    fn = f"signal_analysis__rois_{roi_type.name}__channel_{raw_channel}.csv"
-                    outfile: Path = Path(H.analysis_path) / fn
-                    logging.info("Writing output file: %s", outfile)
-                    stats_frame.to_csv(outfile, index=False)
-    
+
+@curry_flip(1)
+def traverse_through_either(inputs: Iterable[_A], f: Callable[[_A], Result[_B, str]]) -> Result[Seq[_B], Seq[str]]:
+    State: TypeAlias = Result[Seq[_B], Seq[str]]
+
+    def proc1(acc: State, a: _A) -> State:
+        match acc, f(a):
+            case result.Result(tag="ok", ok=goods), result.Result(tag="ok", ok=b):
+                return Result.Ok(goods.append(Seq.of(b))) # new good result
+            case result.Result(tag="ok", ok=_), result.Result(tag="error", error=err):
+                return Result.Error(Seq.of(err)) # first error
+            case result.Result(tag="error", error=bads), result.Result(tag="ok", ok=_):
+                return Result.Error(bads) # no new error
+            case result.Result(tag="error", error=bads), result.Result(tag="error", error=err):
+                return Result.Error(bads.append(Seq.of(err))) # new error
+
+    return Seq.of_iterable(inputs).fold(proc1, Result.Ok(Seq()))
+
 
 def _ensure_unique(items: Iterable[_A]) -> Result[set[_A], str]:
     try:
@@ -206,22 +221,9 @@ def _ensure_unique(items: Iterable[_A]) -> Result[set[_A], str]:
 
 
 def _parse_imaging_channels(channels: object) -> Result[list[ImagingChannel], Seq[str]]:
-    State: TypeAlias = Result[Seq[ImagingChannel], Seq[str]]
-    
-    def proc1(acc: State, obj: object) -> State:
-        match acc, _unsafe_parse_imaging_channel(obj):
-            case result.Result(tag="ok", ok=goods), result.Result(tag="ok", ok=ch):
-                return Result.Ok(goods.append(Seq.of(ch)))
-            case result.Result(tag="ok", ok=_), result.Result(tag="error", error=err):
-                return Result.Error(Seq.of(err)) # first error
-            case result.Result(tag="error", error=bads), result.Result(tag="ok", ok=_):
-                return Result.Error(bads)
-            case result.Result(tag="error", error=bads), result.Result(tag="error", error=err):
-                return Result.Error(bads.append(Seq.of(err)))
-
     return _list_from_object(channels) \
         .map_error(lambda msg: Seq.of(msg)) \
-        .bind(lambda cs: Seq(cs).fold(proc1, Result.Ok(Seq()))) \
+        .bind(traverse_through_either(_unsafe_parse_imaging_channel)) \
         .map(lambda seq: seq.to_list())
 
 
